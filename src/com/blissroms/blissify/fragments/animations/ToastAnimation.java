0c05290db86d0e57de1db5e926f20c9ddd0a5ba4
GwonHyeok <kh4975@naver.com>
2014-03-06 11:31:43 +0100

    XuiMod: Toast Animations [2/2]
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/blissify/fragments/animations/ToastAnimation.java
+++ src/com/blissroms/blissify/fragments/animations/ToastAnimation.java
@@ -1,21 +1,21 @@
 package com.blissroms.blissify.fragments.animations;
 
+import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import android.provider.Settings;
 
 import com.blissroms.blissify.R;
-
-/**
- * Created by jackeagle on 31/12/17.
- */
 
 @SuppressWarnings("DefaultFileTemplate")
 public class ToastAnimation extends Fragment {
@@ -39,18 +39,56 @@ public class ToastAnimation extends Fragment {
         public ToastPreference() {
         }
 
-        private static final String TAG = "TrafficSettingsFragment";
+        private static final String TAG = "ToastPreference";
+        private static final String PREF_TILE_ANIM_STYLE = "anim_tile_style";
+        private static final String PREF_TILE_ANIM_DURATION = "anim_tile_duration";
+        private static final String PREF_TILE_ANIM_INTERPOLATOR = "anim_tile_interpolator";
+        private static final String PREF_TOAST_ANIMATION = "toast_animation";
 
+        private ListPreference mTileAnimationDuration;
+        private ListPreference mTileAnimationInterpolator;
+        private ListPreference mTileAnimationStyle;
+        private ListPreference mToastAnimation;
+        Toast mToast;
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             addPreferencesFromResource(R.xml.toast_animation);
             PreferenceScreen prefSet = getPreferenceScreen();
+            ContentResolver resolver = getActivity().getContentResolver();
+
+            // Toast Animations
+            mToastAnimation = (ListPreference) findPreference(PREF_TOAST_ANIMATION);
+            mToastAnimation.setSummary(mToastAnimation.getEntry());
+            int CurrentToastAnimation = Settings.System.getInt(resolver,
+                    Settings.System.TOAST_ANIMATION, 1);
+            mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+            mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+            mToastAnimation.setOnPreferenceChangeListener(this);
+            if (mToast != null) {
+                mToast.cancel();
+                mToast = null;
+            }
         }
 
         public boolean onPreferenceChange(Preference preference, Object newValue) {
+            int value;
+            int index;
+            ContentResolver resolver = getActivity().getContentResolver();
+            if (preference == mToastAnimation) {
+                index = mToastAnimation.findIndexOfValue((String) newValue);
+                Settings.System.putInt(resolver,
+                        Settings.System.TOAST_ANIMATION, index);
+                mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+                if (mToast != null) {
+                    mToast.cancel();
+                }
+                mToast = Toast.makeText(getActivity(), mToastAnimation.getEntries()[index],
+                        Toast.LENGTH_SHORT);
+                mToast.show();
+                return true;
+            }
             return false;
         }
-
     }
 }
