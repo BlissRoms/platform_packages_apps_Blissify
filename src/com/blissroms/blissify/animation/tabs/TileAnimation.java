5e3e828405d1bac4a0f858618dd58fb685684fae
Nico60 <ndhouailly@gmail.com>
2016-05-16 21:53:07 +0200

    Animations: Add Tile Animations [2/2]
    Change-Id: I2e4009027d35a519a8e8b3b17e41b4dcd829c307
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/blissify/animation/tabs/TileAnimation.java
+++ src/com/blissroms/blissify/animation/tabs/TileAnimation.java
@@ -16,19 +16,38 @@
 
 package com.blissroms.blissify.animation.tabs;
 
+import android.app.ActivityManagerNative;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.preference.ListPreference;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import com.android.internal.logging.MetricsProto.MetricsEvent;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.settings.Utils;
 
 public class TileAnimation extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
