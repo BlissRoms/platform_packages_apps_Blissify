d101190979a35d30e5e47937d184768b0a33eb9c
Jackeagle <jackeagle102@gmail.com>
2016-07-03 16:07:17 +0530

    Blissify: Add Gestures Category - Add Touch Gestures Tab
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ src/com/blissroms/blissify/gestures/tabs/TouchGestures.java
@@ -0,0 +1,50 @@
+/*
+* Copyright (C) 2014 BlissRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.blissroms.blissify.gestures.tabs;
+
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class TouchGestures extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+		
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.touch_gestures_tab);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.BLISSIFY;
+    }
+}
