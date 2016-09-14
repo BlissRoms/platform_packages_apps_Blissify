79a01b5cc08ef2c0e26853ea5b58f5654aaee581
GwonHyeok <kh4975@gmail.com>
2016-09-14 17:57:13 +0530

    Blissify: Animations: SS - XuiMod: Toast Animations [2/2]
    Change-Id: I099ddde06a7ee771615edc13bc48373916e8f1ea
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/blissify/animation/tabs/ToastAnimation.java
+++ src/com/blissroms/blissify/animation/tabs/ToastAnimation.java
@@ -16,38 +16,64 @@
 
 package com.blissroms.blissify.animation.tabs;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.provider.Settings;
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceScreen;
-import android.support.v14.preference.PreferenceFragment;
-import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
+import android.widget.Toast;
 
 import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class ToastAnimation extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        Preference.OnPreferenceChangeListener {
 
-    private static final String TAG = "ToastAnimation";
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.toast_animation);
-    }
-
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return false;
-    }
+      private static final String TAG = "ToastAnimation";
+      private static final String KEY_TOAST_ANIMATION = "toast_animation";
+      private ListPreference mToastAnimation;
+      protected Context mContext;
+      protected ContentResolver mContentRes;
 
     @Override
     protected int getMetricsCategory() {
         return MetricsEvent.BLISSIFY;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.toast_animation);
+		mContext = getActivity().getApplicationContext();
+		mContentRes = getActivity().getContentResolver();
+
+ 		mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+        mToastAnimation.setSummary(mToastAnimation.getEntry());
+        int CurrentToastAnimation = Settings.System.getInt(getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
+        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+        mToastAnimation.setOnPreferenceChangeListener(this);
+    }
+
+	@Override
+        public boolean onPreferenceChange(Preference preference, Object newValue){
+		  boolean result = false;
+           if (preference == mToastAnimation) {
+              int index = mToastAnimation.findIndexOfValue((String) newValue);
+              Settings.System.putString(getContentResolver(), Settings.System.TOAST_ANIMATION, (String) newValue);
+              mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+              Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
+              return true;
+          }
+        return false;
     }
 }
