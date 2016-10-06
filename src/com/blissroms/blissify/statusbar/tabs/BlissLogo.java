bde29d1a2878adab5be360adf4f3eb83e4e785a8
LorDClockaN <davor@losinj.com>
2016-10-06 19:20:00 +0530

    Blissify: Logo: Add Bliss Logo Color Customization [2/2]
    Change-Id: I8a48c190a783503677f76c3038372607c3af7013
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/blissify/statusbar/tabs/BlissLogo.java
+++ src/com/blissroms/blissify/statusbar/tabs/BlissLogo.java
@@ -16,7 +16,12 @@
 
 package com.blissroms.blissify.statusbar.tabs;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
@@ -30,19 +35,47 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import net.margaritov.preference.colorpicker.ColorPickerPreference;
+
 public class BlissLogo extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "BlissLogo";
 
+    private static final String KEY_BLISS_LOGO_COLOR = "status_bar_bliss_logo_color";
+
+    private ColorPickerPreference mBlissLogoColor;
+    private ListPreference mBlissLogoStyle;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.statusbar_logo);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        // Bliss logo color
+        mBlissLogoColor =
+        (ColorPickerPreference) prefSet.findPreference(KEY_BLISS_LOGO_COLOR);
+        mBlissLogoColor.setOnPreferenceChangeListener(this);
+        int intColor = Settings.System.getInt(getContentResolver(),
+        Settings.System.STATUS_BAR_BLISS_LOGO_COLOR, 0xffffffff);
+       	String hexColor = String.format("#%08x", (0xffffffff & intColor));
+        mBlissLogoColor.setSummary(hexColor);
+        mBlissLogoColor.setNewPreviewColor(intColor);
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+	@Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mBlissLogoColor) {
+            String hex = ColorPickerPreference.convertToARGB(
+                    Integer.valueOf(String.valueOf(newValue)));
+            preference.setSummary(hex);
+            int intHex = ColorPickerPreference.convertToColorInt(hex);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_BLISS_LOGO_COLOR, intHex);
+            return true;
+        }
         return false;
     }
 
