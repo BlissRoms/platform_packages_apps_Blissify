c48931570d7cd14189141bfeaa23d63ce5ebf8c4
XXMrHyde <xxmrhyde@gmx.co.uk>
2016-10-14 14:29:24 +0200

    Custom QS rows/columns [2/2]
    *DU Edits*
    - Cleaned up some of the redundancy
    
    Change-Id: I05380b5ce5edb55775f559e1b4bd40a73ded506f

--- src/com/blissroms/blissify/quicksettings/tabs/QSAdvanced.java
+++ src/com/blissroms/blissify/quicksettings/tabs/QSAdvanced.java
@@ -34,15 +34,71 @@ public class QSAdvanced extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "QSAdvanced";
+    private static final String PREF_ROWS_PORTRAIT = "qs_rows_portrait";
+    private static final String PREF_ROWS_LANDSCAPE = "qs_rows_landscape";
+    private static final String PREF_COLUMNS = "qs_columns";
+
+    private ListPreference mRowsPortrait;
+    private ListPreference mRowsLandscape;
+    private ListPreference mQsColumns;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.qs_advanced);
+
+        int defaultValue;
+
+        mRowsPortrait = (ListPreference) findPreference(PREF_ROWS_PORTRAIT);
+        int rowsPortrait = Settings.Secure.getInt(resolver,
+                Settings.Secure.QS_ROWS_PORTRAIT, 3);
+        mRowsPortrait.setValue(String.valueOf(rowsPortrait));
+        mRowsPortrait.setSummary(mRowsPortrait.getEntry());
+        mRowsPortrait.setOnPreferenceChangeListener(this);
+
+        defaultValue = getResources().getInteger(com.android.internal.R.integer.config_qs_num_rows_landscape_default);
+        mRowsLandscape = (ListPreference) findPreference(PREF_ROWS_LANDSCAPE);
+        int rowsLandscape = Settings.Secure.getInt(resolver,
+                Settings.Secure.QS_ROWS_LANDSCAPE, defaultValue);
+        mRowsLandscape.setValue(String.valueOf(rowsLandscape));
+        mRowsLandscape.setSummary(mRowsLandscape.getEntry());
+        mRowsLandscape.setOnPreferenceChangeListener(this);
+
+        mQsColumns = (ListPreference) findPreference(PREF_COLUMNS);
+        int columnsQs = Settings.Secure.getInt(resolver,
+                Settings.Secure.QS_COLUMNS, 3);
+        mQsColumns.setValue(String.valueOf(columnsQs));
+        mQsColumns.setSummary(mQsColumns.getEntry());
+        mQsColumns.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+            int intValue;
+            int index;
+
+            if (preference == mRowsPortrait) {
+                intValue = Integer.valueOf((String) newValue);
+                index = mRowsPortrait.findIndexOfValue((String) newValue);
+                Settings.Secure.putInt(resolver,
+                        Settings.Secure.QS_ROWS_PORTRAIT, intValue);
+                preference.setSummary(mRowsPortrait.getEntries()[index]);
+                return true;
+            } else if (preference == mRowsLandscape) {
+                intValue = Integer.valueOf((String) newValue);
+                index = mRowsLandscape.findIndexOfValue((String) newValue);
+                Settings.Secure.putInt(resolver,
+                        Settings.Secure.QS_ROWS_LANDSCAPE, intValue);
+                preference.setSummary(mRowsLandscape.getEntries()[index]);
+                return true;
+            } else if (preference == mQsColumns) {
+                intValue = Integer.valueOf((String) newValue);
+                index = mQsColumns.findIndexOfValue((String) newValue);
+                Settings.Secure.putInt(resolver,
+                        Settings.Secure.QS_COLUMNS, intValue);
+                preference.setSummary(mQsColumns.getEntries()[index]);
+                return true;
+            }
         return false;
     }
 
