551ca3dc9ab441d48752779ccc69b742f377d173
Alex Cruz <mazdarider23@gmail.com>
2016-11-08 11:30:29 +0530

    ScreenShot Delay [2/2]

--- src/com/blissroms/blissify/advanced/tabs/AdvancedSettings.java
+++ src/com/blissroms/blissify/advanced/tabs/AdvancedSettings.java
@@ -17,6 +17,9 @@
 package com.blissroms.blissify.advanced.tabs;
 
 import android.os.Bundle;
+import android.content.ContentResolver;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
@@ -29,20 +32,38 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.bliss.CustomSeekBarPreference;
 
 public class AdvancedSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String TAG = "AdvancedSettings";
 
+    private static final String SCREENSHOT_DELAY = "screenshot_delay";
+
+    private CustomSeekBarPreference mScreenshotDelay;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
         addPreferencesFromResource(R.xml.advanced_settings);
+
+        mScreenshotDelay = (CustomSeekBarPreference) findPreference(SCREENSHOT_DELAY);
+        int screenshotDelay = Settings.System.getInt(resolver,
+                Settings.System.SCREENSHOT_DELAY, 1000);
+        mScreenshotDelay.setValue(screenshotDelay / 1);
+        mScreenshotDelay.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mScreenshotDelay) {
+            int screenshotDelay = (Integer) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SCREENSHOT_DELAY, screenshotDelay * 1);
+            return true;
+         }
         return false;
     }
 
