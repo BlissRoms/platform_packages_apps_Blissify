7ce7ddc9714aad6413f30c43710e325c5845ea06
Jackeagle <jackeagle102@gmail.com>
2017-01-10 11:00:48 +0530

    Blissify: Animations: Add Toast Animation Tab
    Change-Id: I8de6ace59e3d9aeb00fa16dbfbe02e5eb9f16977
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/blissify/animation/AnimationSettings.java
+++ src/com/blissroms/blissify/animation/AnimationSettings.java
@@ -35,6 +35,7 @@ import android.view.ViewGroup;
 
 import com.blissroms.blissify.PagerSlidingTabStrip;
 import com.blissroms.blissify.animation.tabs.SystemAnimation;
+import com.blissroms.blissify.animation.tabs.ToastAnimation;
 import com.android.settings.R;
 import com.android.settings.Utils;
 import com.android.settings.SettingsPreferenceFragment;
@@ -93,6 +94,7 @@ public class AnimationSettings extends SettingsPreferenceFragment {
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
 	    frags[0] = new SystemAnimation();
+	    frags[1] = new ToastAnimation();
         }
 
         @Override
@@ -114,7 +116,8 @@ public class AnimationSettings extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-		    getString(R.string.system_animation_title)};
+		    getString(R.string.system_animation_title),
+		    getString(R.string.toast_animation_title)};
         return titleString;
     }
 
