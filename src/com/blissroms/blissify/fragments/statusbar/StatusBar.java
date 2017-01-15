68affbf534c9058c607fe9c50cdbe86f0fa49a56
Hendrik Hagendorn <finnq@lineageos.org>
2017-01-15 08:31:13 +0100

    Blissify: Bring back and refactor battery icon options
    * Use the icon blacklist for hiding
    * Combine options for hiding status bar icons in system tuner settings
    
    Change-Id: Id8c6df982192cfb54350c9cfe8d8ca4983dd7972

--- src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
+++ src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
@@ -74,11 +74,14 @@ public class StatusBar extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String CATEGORY_BATTERY = "status_bar_battery_key";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
 
     private LineageSystemSettingListPreference mStatusBarBattery;
     private LineageSystemSettingListPreference mStatusBarBatteryShowPercent;
     private PreferenceCategory mStatusBarBatteryCategory;
 
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
 
     private static final String CATEGORY_CLOCK = "status_bar_clock_key";
 
@@ -130,6 +133,13 @@ public class StatusBar extends SettingsPreferenceFragment
 
         mStatusBarBatteryCategory =
                 (PreferenceCategory) prefSet.findPreference(CATEGORY_BATTERY);
+
+        mStatusBarBatteryShowPercent =
+                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        mStatusBarBattery =
+                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        mStatusBarBattery.setOnPreferenceChangeListener(this);
+        enableStatusBarBatteryDependents(mStatusBarBattery.getIntValue(2));
 /*
         mStatusBarClock =
                 (LineageSystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
@@ -261,7 +271,18 @@ public class StatusBar extends SettingsPreferenceFragment
             return true;
         }
 */
-        return false;
+        int value = Integer.parseInt((String) newValue);
+        String key = preference.getKey();
+        switch (key) {
+            case STATUS_BAR_BATTERY_STYLE:
+                enableStatusBarBatteryDependents(value);
+                break;
+        }
+        return true;
+    }
+
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        mStatusBarBatteryShowPercent.setEnabled(batteryIconStyle != STATUS_BAR_BATTERY_STYLE_TEXT);
     }
 
 /*    private int getClockPosition() {
