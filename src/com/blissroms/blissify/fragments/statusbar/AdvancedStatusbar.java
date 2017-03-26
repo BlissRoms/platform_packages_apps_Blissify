f17eaeaed6762385dc7903cef2683e72435c77b5
Pranav Vashi <neobuddy89@gmail.com>
2017-03-26 22:07:39 +0530

    Ability to toggle VoLTE icon in statusbar [2/2]
    Change-Id: I8011f29a5a850ff0dc1c7817785c0e9ec523a531
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/blissroms/blissify/fragments/statusbar/AdvancedStatusbar.java
+++ src/com/blissroms/blissify/fragments/statusbar/AdvancedStatusbar.java
@@ -55,6 +55,10 @@ import java.util.Collections;
 public class Breathing extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String KEY_SHOW_VOLTE = "show_volte_icon";
+
+    private SwitchPreference mShowVolte;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -62,12 +66,27 @@ public class Breathing extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.statusbar_advanced_icons);
         PreferenceScreen prefSet = getPreferenceScreen();
 
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mShowVolte = (SwitchPreference) findPreference(KEY_SHOW_VOLTE);
+
+        if (!TelephonyUtils.isVoiceCapable(getActivity())) {
+            prefScreen.removePreference(mShowVolte);
+        }
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
         return false;
+    }
+
+    public static void reset(Context mContext) {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        Settings.System.putIntForUser(resolver,
+                Settings.System.SHOW_VOLTE_ICON, 0, UserHandle.USER_CURRENT);
     }
 
     @Override
