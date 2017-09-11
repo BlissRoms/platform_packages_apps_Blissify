f6dd609082c177a17de5b43369783062ac73896d
ezio84 <brabus84@gmail.com>
2017-09-11 06:46:31 +0530

    Battery light: Add customization [2/2]
    aex edits: Add back disable battery light toggle
    for obvious reason, also set other options dependencies
    on it
    
    Change-Id: I792c6428a774fb92b981e4e248d024c45acd6f4e
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
+++ src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
@@ -73,6 +73,8 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private ListPreference mNetTrafficType;
     private SystemSettingSwitchPreference mShowArrows;
     private CustomSeekBarPreference mNetTrafficSize;
+    private PreferenceCategory mLedsCategory;
+    private Preference mChargingLeds;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -82,6 +84,17 @@ public class StatusBar extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.blissify_statusbar);
         setRetainInstance(true);
         PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLedsCategory = (PreferenceCategory) findPreference("light_category");
+        mChargingLeds = (Preference) findPreference("battery_charging_light");
+        if (mChargingLeds != null
+                && !getResources().getBoolean(
+                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
+            mLedsCategory.removePreference(mChargingLeds);
+        }
+          if (mChargingLeds == null) {
+            prefSet.removePreference(mLedsCategory);
+        }
 
         int NetTrafficSize = Settings.System.getInt(resolver,
                 Settings.System.NETWORK_TRAFFIC_FONT_SIZE, 42);
