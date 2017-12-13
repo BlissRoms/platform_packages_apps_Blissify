c0df14c14860afa27570823d13925ef38f397a84
Pranav Vashi <neobuddy89@gmail.com>
2017-12-14 01:39:07 +0530

    Add ability to toggle bluetooth battery level in QS [2/2]
    Change-Id: I5b8d77b2239a839c84e6a1adbea9cdb1fdb784a2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- src/com/blissroms/blissify/fragments/qs/QuickSettings.java
+++ src/com/blissroms/blissify/fragments/qs/QuickSettings.java
@@ -84,6 +84,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 Settings.System.QS_BATTERY_MODE, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.SHOW_QS_CLOCK, 1, UserHandle.USER_CURRENT);
+        Settings.System.putInt(resolver,
+                Settings.System.BLUETOOTH_QS_SHOW_BATTERY, 1);
     }
 
     @Override
