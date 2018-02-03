f243ec6795aec40d90956001031cc58628a03fe0
Pranav Vashi <neobuddy89@gmail.com>
2018-02-03 18:07:19 +0530

    Statusbar clock customizations (2/2)
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If689df1098c14078478743e390325d9aab174ebe

--- src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
+++ src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
 package com.blissroms.blissify.fragments.statusbar;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -44,6 +43,7 @@ import lineageos.preference.LineageSecureSettingSwitchPreference;
 import lineageos.providers.LineageSettings;
 
 import lineageos.preference.LineageSystemSettingListPreference;
+import com.blissroms.blissify.fragments.statusbar.Clock;
 
 import java.util.Locale;
 import android.text.TextUtils;
@@ -66,12 +66,10 @@ public class StatusBar extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     private static final String CATEGORY_BATTERY = "status_bar_battery_key";
-    private static final String CATEGORY_CLOCK = "status_bar_clock_key";
 
     private static final String ICON_BLACKLIST = "icon_blacklist";
 
     private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock";
-    private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
     private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
     private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
@@ -91,16 +89,15 @@ public class StatusBar extends SettingsPreferenceFragment
     private LineageSystemSettingListPreference mStatusBarBatteryShowPercent;
 
     private PreferenceCategory mStatusBarBatteryCategory;
-    private PreferenceCategory mStatusBarClockCategory;
     private PreferenceScreen mNetworkTrafficPref;
 
     private static boolean sHasNotch;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.blissify_statusbar);
-        
+
         mNetworkTrafficPref = (PreferenceScreen) findPreference(NETWORK_TRAFFIC_SETTINGS);
 
         sHasNotch = getResources().getBoolean(
@@ -110,8 +107,6 @@ public class StatusBar extends SettingsPreferenceFragment
             getPreferenceScreen().removePreference(mNetworkTrafficPref);
         }
 
-        mStatusBarAmPm =
-                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_AM_PM);
         mStatusBarClock =
                 (LineageSystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_STYLE);
         mStatusBarClock.setOnPreferenceChangeListener(this);
@@ -140,6 +135,9 @@ public class StatusBar extends SettingsPreferenceFragment
     public void onResume() {
         super.onResume();
 
+        sHasNotch = getResources().getBoolean(
+                org.lineageos.platform.internal.R.bool.config_haveNotch);
+
         final String curIconBlacklist = Settings.Secure.getString(getContext().getContentResolver(),
                 ICON_BLACKLIST);
 
@@ -153,11 +151,6 @@ public class StatusBar extends SettingsPreferenceFragment
             getPreferenceScreen().removePreference(mStatusBarBatteryCategory);
         } else {
             getPreferenceScreen().addPreference(mStatusBarBatteryCategory);
-        }
-
-        if (DateFormat.is24HourFormat(getActivity())) {
-            mStatusBarAmPm.setEnabled(false);
-            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_info);
         }
 
         final boolean disallowCenteredClock = sHasNotch || getNetworkTrafficStatus() != 0;
@@ -183,6 +176,7 @@ public class StatusBar extends SettingsPreferenceFragment
 
         // Disable network traffic preferences if clock is centered in the status bar
         updateNetworkTrafficStatus(getClockPosition());
+
     }
 
     @Override
@@ -192,9 +186,6 @@ public class StatusBar extends SettingsPreferenceFragment
         switch (key) {
             case STATUS_BAR_QUICK_QS_PULLDOWN:
                 updateQuickPulldownSummary(value);
-                break;
-            case STATUS_BAR_CLOCK_STYLE:
-                updateNetworkTrafficStatus(value);
                 break;
             case STATUS_BAR_BATTERY_STYLE:
                 enableStatusBarBatteryDependents(value);
