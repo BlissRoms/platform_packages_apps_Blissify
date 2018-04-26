8836761599a391295ff8e189e0980957700ad977
Jackeagle <jackeagle102@gmail.com>
2018-04-26 22:11:58 +0530

    Blissify: Add TabHolder to various Categories - Gestures - Lockscreen - PowerMenu - QuickSettings - Recents - StatusBar - Remove useless Nav Drawer options
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- app/src/main/java/com/blissroms/blissify/categories/Recents.java
+++ app/src/main/java/com/blissroms/blissify/categories/Recents.java
@@ -1,11 +1,82 @@
 package com.blissroms.blissify.categories;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.blissroms.blissify.R;
 
 /**
- * Created by jackeagle on 1/1/18.
+ * Created by jackeagle on 31/12/17.
  */
 
 @SuppressWarnings("DefaultFileTemplate")
 public class Recents extends Fragment {
+
+    private View view;
+    private ViewPager viewPager;
+    private TabLayout tableLayout;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.blissify_tablayout, container, false);
+
+        viewPager = view.findViewById(R.id.viewpager);
+        viewPager.setAdapter(new RecentsAdapter(getChildFragmentManager()));
+        tableLayout = view.findViewById(R.id.sliding_tabs);
+        tableLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tableLayout.setupWithViewPager(viewPager);
+            }
+        });
+
+        return view;
+    }
+
+    private class RecentsAdapter extends FragmentPagerAdapter {
+
+        final String[] tabs= getTabsTitle();
+        private final Fragment[] frags = new Fragment[tabs.length];
+
+        public RecentsAdapter(FragmentManager fm) {
+            super(fm);
+            // Add Fragments Here
+
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return frags[position];
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabs[position];
+        }
+
+        // Tab Titles
+        private String[] getTabsTitle() {
+            String titleString[];
+            titleString = new String[]
+                    {
+                      // Add Tab Fragment Title
+
+                    };
+            return titleString;
+        }
+    }
 }
