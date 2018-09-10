c66de05fb91ee0e219990aa89f5f36e2fb49a932
Jackeagle <jackeagle102@gmail.com>
2018-09-09 23:58:10 -0400

    Blissify: Cleanup all gradle stuffs and Build with source
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -13,6 +13,41 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
 
-include $(LOCAL_PATH)/app/src/main/Android.mk
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
+LOCAL_PROGUARD_ENABLED := disabled
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    $(ANDROID_SUPPORT_DESIGN_TARGETS) \
+    android-support-compat \
+    android-support-v4 \
+    android-support-v13 \
+    android-support-v7-appcompat \
+    android-support-v7-cardview \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v14-preference \
+    android-support-transition \
+    android-support-v7-palette \
+
+LOCAL_USE_AAPT2 := true
+LOCAL_AAPT_FLAGS := \
+    --auto-add-overlay
+
+LOCAL_PACKAGE_NAME := Blissify
+
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under, $(LOCAL_PATH))
