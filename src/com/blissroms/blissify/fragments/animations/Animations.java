aeb591cfed36e30b19ab20574a3aae5f680e6a59
cristianomatos <cristianobmatos@gmail.com>
2018-09-13 21:03:29 +0530

    Blissify: AOKP animations: Add an entry for TRANSIT_TASK_OPEN_BEHIND
    Already in frameworks/base. Option was just missing from Settings
    
    Change-Id: I587a79c7f71dd18a045618f12330ccf68aea11a7
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- src/com/blissroms/blissify/fragments/animations/Animations.java
+++ src/com/blissroms/blissify/fragments/animations/Animations.java
@@ -59,6 +59,7 @@ public class Animations extends SettingsPreferenceFragment implements
     private static final String ACTIVITY_OPEN = "activity_open";
     private static final String ACTIVITY_CLOSE = "activity_close";
     private static final String TASK_OPEN = "task_open";
+    private static final String TASK_OPEN_BEHIND = "task_open_behind";
     private static final String TASK_CLOSE = "task_close";
     private static final String TASK_TO_FRONT = "task_to_front";
     private static final String TASK_TO_BACK = "task_to_back";
@@ -76,6 +77,7 @@ public class Animations extends SettingsPreferenceFragment implements
     ListPreference mActivityOpenPref;
     ListPreference mActivityClosePref;
     ListPreference mTaskOpenPref;
+    ListPreference mTaskOpenBehind;
     ListPreference mTaskClosePref;
     ListPreference mTaskMoveToFrontPref;
     ListPreference mTaskMoveToBackPref;
@@ -147,6 +149,12 @@ public class Animations extends SettingsPreferenceFragment implements
         mTaskOpenPref.setSummary(getProperSummary(mTaskOpenPref));
         mTaskOpenPref.setEntries(mAnimationsStrings);
         mTaskOpenPref.setEntryValues(mAnimationsNum);
+
+        mTaskOpenBehind = (ListPreference) findPreference(TASK_OPEN_BEHIND);
+        mTaskOpenBehind.setOnPreferenceChangeListener(this);
+        mTaskOpenBehind.setSummary(getProperSummary(mTaskOpenBehind));
+        mTaskOpenBehind.setEntries(mAnimationsStrings);
+        mTaskOpenBehind.setEntryValues(mAnimationsNum);
 
         mTaskClosePref = (ListPreference) findPreference(TASK_CLOSE);
         mTaskClosePref.setOnPreferenceChangeListener(this);
@@ -258,6 +266,10 @@ public class Animations extends SettingsPreferenceFragment implements
             int val = Integer.parseInt((String) newValue);
             result = Settings.Global.putInt(mContext.getContentResolver(),
                     Settings.Global.ACTIVITY_ANIMATION_CONTROLS[9], val);
+        } else if (preference == mTaskOpenBehind) {
+            int val = Integer.parseInt((String) newValue);
+            result = Settings.Global.putInt(mContext.getContentResolver(),
+                    Settings.Global.ACTIVITY_ANIMATION_CONTROLS[10], val);
         }
         preference.setSummary(getProperSummary(preference));
         return result;
@@ -285,6 +297,8 @@ public class Animations extends SettingsPreferenceFragment implements
             mString = Settings.Global.ACTIVITY_ANIMATION_CONTROLS[8];
         } else if (preference == mWallpaperIntraClose) {
             mString = Settings.Global.ACTIVITY_ANIMATION_CONTROLS[9];
+        } else if (preference == mTaskOpenBehind) {
+            mString = Settings.Global.ACTIVITY_ANIMATION_CONTROLS[10];
         }
         int mNum = Settings.Global.getInt(mContext.getContentResolver(), mString, 0);
         return mAnimationsStrings[mNum];
