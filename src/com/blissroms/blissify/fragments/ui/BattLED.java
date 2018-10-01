0dd2d8fc256f68d6d7a4db6f465d5ee0dc82e2a6
Marko Man <darkobas@gmail.com>
2018-10-01 11:57:33 -0600

    Blissify: Battery LED Settings
    Change-Id: Iaf630a9f4faecf9ee857822eb69e571b5d2f4679

--- src/com/blissroms/blissify/fragments/ui/BattLED.java
+++ src/com/blissroms/blissify/fragments/ui/BattLED.java
@@ -9,9 +9,11 @@ import android.content.res.Resources;
 import android.database.ContentObserver;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.ListPreference;
@@ -57,16 +59,146 @@ public class BattLED extends Fragment {
         public SystemPreference() {
         }
 
+    private ColorPickerPreference mLowColor;
+    private ColorPickerPreference mMediumColor;
+    private ColorPickerPreference mFullColor;
+    private ColorPickerPreference mReallyFullColor;
+    private ColorPickerPreference mFastLightColor;
+    private SystemSettingSwitchPreference mLowBatteryBlinking;
+    private SystemSettingSwitchPreference mFastColorLight;
+
+    private PreferenceCategory mColorCategory;
+    private PreferenceCategory mFastColorCategory;
+
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             addPreferencesFromResource(R.xml.interface_batteryled);
             PreferenceScreen prefSet = getPreferenceScreen();
             ContentResolver resolver = getActivity().getContentResolver();
+
+            mColorCategory = (PreferenceCategory) findPreference("battery_light_cat");
+            mFastColorCategory = (PreferenceCategory) findPreference("fast_color_cat");
+
+            mLowBatteryBlinking = (SystemSettingSwitchPreference)prefSet.findPreference("battery_light_pulse");
+            if (getResources().getBoolean(
+                            com.android.internal.R.bool.config_intrusiveBatteryLed)) {
+                mLowBatteryBlinking.setChecked(Settings.System.getIntForUser(resolver,
+                                Settings.System.OMNI_BATTERY_LIGHT_LOW_BLINKING, 0, UserHandle.USER_CURRENT) == 1);
+                mLowBatteryBlinking.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mLowBatteryBlinking);
+          }
+
+            if (getResources().getBoolean(com.android.internal.R.bool.config_multiColorBatteryLed)) {
+                int color = Settings.System.getIntForUser(resolver,
+                        Settings.System.OMNI_BATTERY_LIGHT_LOW_COLOR, 0xFFFF0000,
+                                UserHandle.USER_CURRENT);
+                mLowColor = (ColorPickerPreference) findPreference("low_color");
+                mLowColor.setAlphaSliderEnabled(false);
+                mLowColor.setNewPreviewColor(color);
+                mLowColor.setOnPreferenceChangeListener(this);
+
+                color = Settings.System.getIntForUser(resolver,
+                        Settings.System.OMNI_BATTERY_LIGHT_MEDIUM_COLOR, 0xFFFFFF00,
+                                UserHandle.USER_CURRENT);
+                mMediumColor = (ColorPickerPreference) findPreference("medium_color");
+                mMediumColor.setAlphaSliderEnabled(false);
+                mMediumColor.setNewPreviewColor(color);
+                mMediumColor.setOnPreferenceChangeListener(this);
+
+                color = Settings.System.getIntForUser(resolver,
+                        Settings.System.OMNI_BATTERY_LIGHT_FULL_COLOR, 0xFFFFFF00,
+                                UserHandle.USER_CURRENT);
+                mFullColor = (ColorPickerPreference) findPreference("full_color");
+                mFullColor.setAlphaSliderEnabled(false);
+                mFullColor.setNewPreviewColor(color);
+                mFullColor.setOnPreferenceChangeListener(this);
+
+                color = Settings.System.getIntForUser(resolver,
+                        Settings.System.OMNI_BATTERY_LIGHT_REALLY_FULL_COLOR, 0xFF00FF00,
+                                UserHandle.USER_CURRENT);
+                mReallyFullColor = (ColorPickerPreference) findPreference("really_full_color");
+                mReallyFullColor.setAlphaSliderEnabled(false);
+                mReallyFullColor.setNewPreviewColor(color);
+                mReallyFullColor.setOnPreferenceChangeListener(this);
+            } else {
+                prefSet.removePreference(mColorCategory);
+            }
+            mFastColorLight = (SystemSettingSwitchPreference)prefSet.findPreference("fast_charging_led_enabled");
+            if (getResources().getBoolean(com.android.internal.R.bool.config_FastChargingLedSupported)) {
+
+                mFastColorLight.setChecked(Settings.System.getIntForUser(resolver,
+                                Settings.System.OMNI_FAST_CHARGING_LED_ENABLED, 0, UserHandle.USER_CURRENT) == 1);
+                mFastColorLight.setOnPreferenceChangeListener(this);
+
+                int color = Settings.System.getIntForUser(resolver,
+                        Settings.System.OMNI_FAST_BATTERY_LIGHT_COLOR, 0xFFFF0000,
+                                UserHandle.USER_CURRENT);
+                mFastLightColor = (ColorPickerPreference) findPreference("fast_color");
+                mFastLightColor.setAlphaSliderEnabled(false);
+                mFastLightColor.setNewPreviewColor(color);
+                mFastLightColor.setOnPreferenceChangeListener(this);
+
+            } else {
+                prefSet.removePreference(mFastColorCategory);
+            }
         }
 
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            ContentResolver resolver = getActivity().getContentResolver();
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference.equals(mLowColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.OMNI_BATTERY_LIGHT_LOW_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mMediumColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.OMNI_BATTERY_LIGHT_MEDIUM_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mFullColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.OMNI_BATTERY_LIGHT_FULL_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mReallyFullColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.OMNI_BATTERY_LIGHT_REALLY_FULL_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference.equals(mFastLightColor)) {
+            int color = ((Integer) newValue).intValue();
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.OMNI_FAST_BATTERY_LIGHT_COLOR, color,
+                    UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mLowBatteryBlinking) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.OMNI_BATTERY_LIGHT_LOW_BLINKING, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mLowBatteryBlinking.setChecked(value);
+            return true;
+        } else if (preference == mFastColorLight) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.OMNI_FAST_CHARGING_LED_ENABLED, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mFastColorLight.setChecked(value);
+            return true;
+        } else if (preference == mLowBatteryBlinking) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.OMNI_BATTERY_LIGHT_LOW_BLINKING, value ? 1 : 0,
+                    UserHandle.USER_CURRENT);
+            mLowBatteryBlinking.setChecked(value);
+            return true;
+        }
         return false;
         }
      }
