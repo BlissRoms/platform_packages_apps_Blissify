b1f8bfc00adf598de8818e807e242a0e9a980335
pimpmaneaton <shaunsibu@gmail.com>
2018-10-09 09:30:52 -0600

    Create Headsup tab in Statusbar category
    Change-Id: I9ee380a925ca4a20dac2741174538756664b6a73

---
+++ src/com/blissroms/blissify/fragments/statusbar/Headsup.java
@@ -0,0 +1,61 @@
+package com.blissroms.blissify.fragments.statusbar;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.ListPreference;
+import android.telephony.TelephonyManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.text.format.DateFormat;
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import com.blissroms.blissify.preference.SystemSettingSwitchPreference;
+import android.provider.Settings;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
+
+public class Headsup extends SettingsPreferenceFragment 
+                                         implements Preference.OnPreferenceChangeListener{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+            addPreferencesFromResource(R.xml.statusbar_headsup);
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+        }
+
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            return false;
+        }
+
+       @Override
+         public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.BLISSIFY;
+       }
+}
