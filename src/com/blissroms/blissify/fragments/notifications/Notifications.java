1454ebd7a00ab6213e247ef0363b39a3784067ef
garwedgess <garethwilliams21@gmail.com>
2018-11-17 20:43:27 +0530

    Blissify: Breathing missedcall/sms/voicemail [5/5]
    Forward ported from N to P to Q
    
    Change-Id: I7dae81ecef5f61723f9e7e58cca07d5f8178cd8a
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/blissroms/blissify/fragments/notifications/Notifications.java
+++ src/com/blissroms/blissify/fragments/notifications/Notifications.java
@@ -28,6 +28,7 @@ import android.content.ContentResolver;
 import android.content.res.Resources;
 import android.provider.Settings;
 import com.android.settings.R;
+import android.net.ConnectivityManager;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -36,6 +37,8 @@ import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
+import com.bliss.support.preferences.CustomSeekBarPreference;
+import com.bliss.support.preferences.SystemSettingSwitchPreference;
 
 import java.util.Locale;
 import android.text.TextUtils;
@@ -58,7 +61,13 @@ public class Notifications extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String HEADS_UP_NOTIFICATIONS_ENABLED = "heads_up_notifications_enabled";
+    private static final String SMS_BREATH = "sms_breath";
+    private static final String MISSED_CALL_BREATH = "missed_call_breath";
+    private static final String VOICEMAIL_BREATH = "voicemail_breath";
 
+    private SwitchPreference mSmsBreath;
+    private SwitchPreference mMissedCallBreath;
+    private SwitchPreference mVoicemailBreath;
     private GlobalSettingMasterSwitchPreference mHeadsUpEnabled;
 
     @Override
@@ -74,6 +83,32 @@ public class Notifications extends SettingsPreferenceFragment implements
         int headsUpEnabled = Settings.Global.getInt(getContentResolver(),
                 HEADS_UP_NOTIFICATIONS_ENABLED, 1);
         mHeadsUpEnabled.setChecked(headsUpEnabled != 0);
+        
+        // Breathing Notifications
+        mSmsBreath = (SwitchPreference) findPreference(SMS_BREATH);
+        mMissedCallBreath = (SwitchPreference) findPreference(MISSED_CALL_BREATH);
+        mVoicemailBreath = (SwitchPreference) findPreference(VOICEMAIL_BREATH);
+
+        ConnectivityManager cm = (ConnectivityManager)
+                getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE)) {
+            mSmsBreath.setChecked(Settings.Global.getInt(resolver,
+                    Settings.Global.KEY_SMS_BREATH, 0) == 1);
+            mSmsBreath.setOnPreferenceChangeListener(this);
+
+            mMissedCallBreath.setChecked(Settings.Global.getInt(resolver,
+                    Settings.Global.KEY_MISSED_CALL_BREATH, 0) == 1);
+            mMissedCallBreath.setOnPreferenceChangeListener(this);
+
+            mVoicemailBreath.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1);
+            mVoicemailBreath.setOnPreferenceChangeListener(this);
+        } else {
+            prefSet.removePreference(mSmsBreath);
+            prefSet.removePreference(mMissedCallBreath);
+            prefSet.removePreference(mVoicemailBreath);
+        }
     }
 
     @Override
@@ -89,6 +124,18 @@ public class Notifications extends SettingsPreferenceFragment implements
             Settings.Global.putInt(getContentResolver(),
 		            HEADS_UP_NOTIFICATIONS_ENABLED, value ? 1 : 0);
             return true;
+        } else if (preference == mSmsBreath) {
+                boolean value = (Boolean) objValue;
+                Settings.Global.putInt(getContentResolver(), SMS_BREATH, value ? 1 : 0);
+                return true;
+        } else if (preference == mMissedCallBreath) {
+                boolean value = (Boolean) objValue;
+                Settings.Global.putInt(getContentResolver(), MISSED_CALL_BREATH, value ? 1 : 0);
+                return true;
+        } else if (preference == mVoicemailBreath) {
+                boolean value = (Boolean) objValue;
+                Settings.System.putInt(getContentResolver(), VOICEMAIL_BREATH, value ? 1 : 0);
+                return true;
         }
         return false;
     }
