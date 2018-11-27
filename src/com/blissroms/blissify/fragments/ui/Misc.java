0566a38d8a454d0a78cb9a71601811fc94e382c7
AgentFabulous <kshitijgm@gmail.com>
2018-11-27 15:01:21 -0700

    [SQUASHED] Rounded Courners [2/2]
    Change-Id: If7000263c58f0830601d28b11b1aeac1851e2332

--- src/com/blissroms/blissify/fragments/ui/Misc.java
+++ src/com/blissroms/blissify/fragments/ui/Misc.java
@@ -38,72 +38,99 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.blissroms.blissify.preference.AppMultiSelectListPreference;
 import com.blissroms.blissify.preference.ScrollAppsViewPreference;
 import com.blissroms.blissify.preference.SystemSettingSwitchPreference;
+import com.blissroms.blissify.preference.SystemSettingSeekBarPreference;
 
 public class Misc extends SettingsPreferenceFragment
                                          implements Preference.OnPreferenceChangeListener{
 
 
-        private static final String TAG = "Misc";
-        private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
-        private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
-        private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
-        private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+    private static final String TAG = "Misc";
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+    private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
+    private static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
 
-        private AppMultiSelectListPreference mAspectRatioAppsSelect;
-        private ScrollAppsViewPreference mAspectRatioApps;
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
+    private SystemSettingSeekBarPreference mContentPadding;
+    private SystemSettingSeekBarPreference mCornerRadius;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-            addPreferencesFromResource(R.xml.interface_misc);
-            PreferenceScreen prefSet = getPreferenceScreen();
-            ContentResolver resolver = getActivity().getContentResolver();
+        addPreferencesFromResource(R.xml.interface_misc);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
-            final PreferenceCategory aspectRatioCategory =
-                    (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
-            final boolean supportMaxAspectRatio = getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
-            if (!supportMaxAspectRatio) {
-                getPreferenceScreen().removePreference(aspectRatioCategory);
+        mContentPadding = (SystemSettingSeekBarPreference) findPreference(SYSUI_ROUNDED_CONTENT_PADDING);
+        int contentPadding = Settings.Secure.getInt(resolver,
+                Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, 1);
+                mContentPadding.setValue(contentPadding / 1);
+                mContentPadding.setOnPreferenceChangeListener(this);
+
+        mCornerRadius = (SystemSettingSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
+        int cornerRadius = Settings.Secure.getInt(resolver,
+                Settings.Secure.SYSUI_ROUNDED_SIZE, 1);
+                mCornerRadius.setValue(cornerRadius / 1);
+                mCornerRadius.setOnPreferenceChangeListener(this);
+
+        final PreferenceCategory aspectRatioCategory =
+                (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+        final boolean supportMaxAspectRatio = getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+        if (!supportMaxAspectRatio) {
+            getPreferenceScreen().removePreference(aspectRatioCategory);
+        } else {
+            mAspectRatioAppsSelect = (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+            mAspectRatioApps = (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+            final String valuesString = Settings.System.getString(resolver, Settings.System.OMNI_ASPECT_RATIO_APPS_LIST);
+            List<String> valuesList = new ArrayList<String>();
+            if (!TextUtils.isEmpty(valuesString)) {
+                valuesList.addAll(Arrays.asList(valuesString.split(":")));
+                mAspectRatioApps.setVisible(true);
+                mAspectRatioApps.setValues(valuesList);
             } else {
-                mAspectRatioAppsSelect = (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
-                mAspectRatioApps = (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
-                final String valuesString = Settings.System.getString(resolver, Settings.System.OMNI_ASPECT_RATIO_APPS_LIST);
-                List<String> valuesList = new ArrayList<String>();
-                if (!TextUtils.isEmpty(valuesString)) {
-                    valuesList.addAll(Arrays.asList(valuesString.split(":")));
-                    mAspectRatioApps.setVisible(true);
-                    mAspectRatioApps.setValues(valuesList);
-                } else {
-                    mAspectRatioApps.setVisible(false);
-                }
-                mAspectRatioAppsSelect.setValues(valuesList);
-                mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
-            }
-        }
-
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            ContentResolver resolver = getActivity().getContentResolver();
-            if (preference == mAspectRatioAppsSelect) {
-                Collection<String> valueList = (Collection<String>) newValue;
                 mAspectRatioApps.setVisible(false);
-                if (valueList != null) {
-                    Settings.System.putString(resolver, Settings.System.OMNI_ASPECT_RATIO_APPS_LIST,
-                            TextUtils.join(":", valueList));
-                    mAspectRatioApps.setVisible(true);
-                    mAspectRatioApps.setValues(valueList);
-                } else {
-                    Settings.System.putString(resolver, Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, "");
-                }
-                return true;
             }
-            return false;
-         }
+            mAspectRatioAppsSelect.setValues(valuesList);
+            mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
+        }
+    }
 
-       @Override
-         public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.BLISSIFY;
-       }
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mAspectRatioAppsSelect) {
+            Collection<String> valueList = (Collection<String>) newValue;
+            mAspectRatioApps.setVisible(false);
+            if (valueList != null) {
+                Settings.System.putString(resolver, Settings.System.OMNI_ASPECT_RATIO_APPS_LIST,
+                        TextUtils.join(":", valueList));
+                mAspectRatioApps.setVisible(true);
+                mAspectRatioApps.setValues(valueList);
+            } else {
+                Settings.System.putString(resolver, Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, "");
+            }
+            return true;
+        } else if (preference == mContentPadding) {
+            int value = (Integer) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.SYSUI_ROUNDED_CONTENT_PADDING, value * 1);
+            return true;
+        } else if (preference == mCornerRadius) {
+            int value = (Integer) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.SYSUI_ROUNDED_SIZE, value * 1);
+            return true;
+        }
+        return false;
+     }
+
+   @Override
+     public int getMetricsCategory() {
+    return MetricsProto.MetricsEvent.BLISSIFY;
+   }
 }
