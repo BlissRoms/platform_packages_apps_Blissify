38422cf63ec7b98179bc3918b36dcb9b7fa172d9
Pranav Vashi <neobuddy89@gmail.com>
2019-03-17 17:02:02 +0530

    Lockscreen Date Font Style [2/2]
    Change-Id: I02c50c3ab238a23d35de3773640921be78a317d9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/blissroms/blissify/fragments/lockscreen/LockScreen.java
+++ src/com/blissroms/blissify/fragments/lockscreen/LockScreen.java
@@ -126,6 +126,8 @@ public class LockScreen extends SettingsPreferenceFragment implements
                 Settings.System.LOCKSCREEN_LOCK_ICON, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LOCK_CLOCK_FONT_STYLE, 4, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCK_DATE_FONT_STYLE, 14, UserHandle.USER_CURRENT);
     }
 
     @Override
