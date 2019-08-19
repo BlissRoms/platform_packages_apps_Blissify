b019c476ee13718e41fe537347f776a1bc96551b
Vachounet <vachounet@live.fr>
2019-08-19 12:22:35 -0600

    Blissify: custom fingerprint icon [2/2]
    Change-Id: I5294516c9ea99e65520861454ec8d5bd0b5f1fb4

--- src/com/blissroms/blissify/fragments/lockscreen/LockSecurity.java
+++ src/com/blissroms/blissify/fragments/lockscreen/LockSecurity.java
@@ -6,16 +6,25 @@ import android.content.ContentResolver;
 import android.app.WallpaperManager;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
 import android.provider.Settings;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
 
+import java.io.FileDescriptor;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -32,6 +41,10 @@ public class LockSecurity extends SettingsPreferenceFragment
              implements Preference.OnPreferenceChangeListener{
 
     private static final String FINGERPRINT_VIB = "fingerprint_success_vib";
+    private static final String FINGERPRINT_CUSTOM_ICON = "custom_fingerprint_icon";
+    private static final String FINGERPRINT_CATEGORY = "category_fingerprint_custom_icon";
+    private static final int GET_CUSTOM_FP_ICON = 69;
+    private Preference mFilePicker;
 
     private FingerprintManager mFingerprintManager;
     private SwitchPreference mFingerprintVib;
@@ -45,6 +58,7 @@ public class LockSecurity extends SettingsPreferenceFragment
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         Resources resources = getResources();
+        mFilePicker = (Preference) findPreference(FINGERPRINT_CUSTOM_ICON);
 
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintVib = (SwitchPreference) findPreference(FINGERPRINT_VIB);
@@ -54,6 +68,50 @@ public class LockSecurity extends SettingsPreferenceFragment
             mFingerprintVib.setChecked((Settings.System.getInt(getContentResolver(),
                     Settings.System.FINGERPRINT_SUCCESS_VIB, 1) == 1));
             mFingerprintVib.setOnPreferenceChangeListener(this);
+        }
+
+        boolean isFODDevice = getResources().getBoolean(com.android.internal.R.bool.config_needCustomFODView);
+        if (!isFODDevice){
+            removePreference(FINGERPRINT_CATEGORY);
+        } else {
+            final String customIconURI = Settings.System.getString(getContext().getContentResolver(),
+                Settings.System.CUSTOM_FP_ICON);
+
+            if (!TextUtils.isEmpty(customIconURI)) {
+                setPickerIcon(customIconURI);
+                mFilePicker.setSummary(customIconURI);
+            }
+
+            mFilePicker.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType("image/png");
+
+                    startActivityForResult(intent, GET_CUSTOM_FP_ICON);
+
+                    return true;
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+        Intent resultData) {
+        if (requestCode == GET_CUSTOM_FP_ICON && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                mFilePicker.setSummary(uri.toString());
+                setPickerIcon(uri.toString());
+                Settings.System.putString(getContentResolver(), Settings.System.CUSTOM_FP_ICON,
+                    uri.toString());
+            }
+        } else if (requestCode == GET_CUSTOM_FP_ICON && resultCode == Activity.RESULT_CANCELED) {
+            mFilePicker.setSummary("");
+            mFilePicker.setIcon(new ColorDrawable(Color.TRANSPARENT));
+            Settings.System.putString(getContentResolver(), Settings.System.CUSTOM_FP_ICON, "");
         }
     }
 
@@ -66,6 +124,19 @@ public class LockSecurity extends SettingsPreferenceFragment
             return true;
         }
         return false;
+    }
+
+    private void setPickerIcon(String uri) {
+        try {
+                ParcelFileDescriptor parcelFileDescriptor =
+                    getContext().getContentResolver().openFileDescriptor(Uri.parse(uri), "r");
+                FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                parcelFileDescriptor.close();
+                Drawable d = new BitmapDrawable(getResources(), image);
+                mFilePicker.setIcon(d);
+            }
+            catch (Exception e) {}
     }
 
        @Override
