46095d6917de5ce7876232da4f274e7b7a14e6f5
maxwen <max.weninger@gmail.com>
2019-10-18 14:06:50 -0600

    Blissify: op gesture tweaks [2/2]
    Change-Id: Ieab8ea15befa5fc3907763ae4eb046a622d27ad8

--- src/com/blissroms/blissify/fragments/buttons/OPGesture.java
+++ src/com/blissroms/blissify/fragments/buttons/OPGesture.java
@@ -21,16 +21,8 @@ import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
-import android.telephony.TelephonyManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.text.format.DateFormat;
-import android.text.Spannable;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 
 import android.provider.Settings;
 
@@ -43,52 +35,88 @@ import java.util.List;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.bliss.support.preferences.SystemSettingSwitchPreference;
+import com.bliss.support.preferences.SeekBarPreferenceCham;
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.internal.util.omni.OmniSwitchConstants;
-import com.android.internal.util.omni.PackageUtils;
-import com.android.internal.util.omni.DeviceUtils;
 
-public class OPGesture extends SettingsPreferenceFragment
-                                         implements Preference.OnPreferenceChangeListener{
+public class OPGesture extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener{
 
-        private static final String TAG = "NavBar";
-        private static final String KEYS_SHOW_NAVBAR_KEY = "navbar_visibility";
+    private static final String TAG = "NavBar";
+    private static final String KEYS_SHOW_NAVBAR_KEY = "navbar_visibility";
+    private static final String KEY_SWIPE_LENGTH = "gesture_swipe_length";
+    private static final String KEY_SWIPE_TIMEOUT = "gesture_swipe_timeout";
 
-        private SwitchPreference mEnableNavBar;
+    private SeekBarPreferenceCham mSwipeTriggerLength;
+    private SeekBarPreferenceCham mSwipeTriggerTimeout;
+
+    private SwitchPreference mEnableNavBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.buttons_op_gesture);
-            PreferenceScreen prefSet = getPreferenceScreen();
-            ContentResolver resolver = getActivity().getContentResolver();
 
-            mEnableNavBar = (SwitchPreference) prefSet.findPreference(KEYS_SHOW_NAVBAR_KEY);
-            boolean showNavBarDefault = DeviceUtils.deviceSupportNavigationBar(getActivity());
-            boolean showNavBar = Settings.Secure.getInt(resolver,
-                    Settings.Secure.NAVIGATION_BAR_VISIBLE, showNavBarDefault ? 1 : 0) == 1;
-            mEnableNavBar.setChecked(showNavBar);
+        addPreferencesFromResource(R.xml.buttons_op_gesture);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
 
+        mEnableNavBar = (SwitchPreference) prefSet.findPreference(KEYS_SHOW_NAVBAR_KEY);
+        boolean showNavBarDefault = DeviceUtils.deviceSupportNavigationBar(getActivity());
+        boolean showNavBar = Settings.Secure.getInt(resolver,
+                Settings.Secure.NAVIGATION_BAR_VISIBLE, showNavBarDefault ? 1 : 0) == 1;
+        mEnableNavBar.setChecked(showNavBar);
+
+        mSwipeTriggerLength = (SeekBarPreferenceCham) findPreference(KEY_SWIPE_LENGTH);
+        int value = Settings.System.getInt(getContentResolver(),
+                Settings.System.OMNI_BOTTOM_GESTURE_SWIPE_LIMIT,
+                getSwipeLengthInPixel(getResources().getInteger(com.android.internal.R.integer.config_navgestureswipeminlength)));
+
+        mSwipeTriggerLength.setMin(getSwipeLengthInPixel(40));
+        mSwipeTriggerLength.setMax(getSwipeLengthInPixel(80));
+        mSwipeTriggerLength.setValue(value);
+        mSwipeTriggerLength.setOnPreferenceChangeListener(this);
+
+        mSwipeTriggerTimeout = (SeekBarPreferenceCham) findPreference(KEY_SWIPE_TIMEOUT);
+        value = Settings.System.getInt(getContentResolver(),
+                Settings.System.OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT,
+                getResources().getInteger(com.android.internal.R.integer.config_navgestureswipetimout));
+        mSwipeTriggerTimeout.setValue(value);
+        mSwipeTriggerTimeout.setOnPreferenceChangeListener(this);
+
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference.equals(mSwipeTriggerLength)) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.OMNI_BOTTOM_GESTURE_SWIPE_LIMIT, value);
+            return true;
+        } else if (preference.equals(mSwipeTriggerTimeout)) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.OMNI_BOTTOM_GESTURE_TRIGGER_TIMEOUT, value);
+            return true;
         }
+        return false;
+    }
 
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            ContentResolver resolver = getActivity().getContentResolver();
-            return false;
+    public boolean onPreferenceTreeClick(Preference preference) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mEnableNavBar) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.NAVIGATION_BAR_VISIBLE, checked ? 1:0);
+            return true;
         }
+        return false;
+    }
 
-        public boolean onPreferenceTreeClick(Preference preference) {
-            ContentResolver resolver = getActivity().getContentResolver();
-            if (preference == mEnableNavBar) {
-                boolean checked = ((SwitchPreference)preference).isChecked();
-                Settings.Secure.putInt(resolver,
-                        Settings.Secure.NAVIGATION_BAR_VISIBLE, checked ? 1:0);
-                return true;
-            }
-            return false;
-        }
+    private int getSwipeLengthInPixel(int value) {
+        return Math.round(value * getResources().getDisplayMetrics().density);
+    }
 
-        @Override
-        public int getMetricsCategory() {
-            return MetricsProto.MetricsEvent.BLISSIFY;
-        }
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.BLISSIFY;
+    }
 }
