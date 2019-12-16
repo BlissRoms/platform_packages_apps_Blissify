75480078f565c3cc1c6f4755a97bfaad02014244
00day0 <therandomuser11@gmail.com>
2019-12-16 21:46:12 +0100

    FOD icon picker [2/2]
    TODO: use lineage fod check
    Change-Id: I1924335bec99cdb4ed999ec81baf1b4dc2998527
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

---
+++ src/com/blissroms/blissify/fragments/lockscreen/FODIconPicker.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2019 ExtendedUI
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blissroms.blissify.fragments.lockscreen;
+
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+import com.android.settings.R;
+import com.android.settingslib.Utils;
+import com.android.settingslib.widget.LayoutPreference;
+import android.graphics.Color;
+import android.provider.Settings;
+
+
+public class FODIconPicker extends LayoutPreference {
+
+    private boolean mAllowDividerAbove;
+    private boolean mAllowDividerBelow;
+    private View mRootView;
+    private static ImageButton ButttonOne;
+    private static ImageButton ButttonTwo;
+    private static ImageButton ButttonThree;
+
+    private static final String TAG = "FODIconPicker";
+
+    public FODIconPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0 /* defStyleAttr */);
+    }
+
+    public FODIconPicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.Preference);
+        mAllowDividerAbove = TypedArrayUtils.getBoolean(a, R.styleable.Preference_allowDividerAbove,
+                R.styleable.Preference_allowDividerAbove, false);
+        mAllowDividerBelow = TypedArrayUtils.getBoolean(a, R.styleable.Preference_allowDividerBelow,
+                R.styleable.Preference_allowDividerBelow, false);
+        a.recycle();
+
+        a = context.obtainStyledAttributes(
+                attrs, R.styleable.Preference, defStyleAttr, 0);
+        int layoutResource = a.getResourceId(R.styleable.Preference_android_layout, 0);
+        if (layoutResource == 0) {
+            throw new IllegalArgumentException("LayoutPreference requires a layout to be defined");
+        }
+        a.recycle();
+
+        // Need to create view now so that findViewById can be called immediately.
+        final View view = LayoutInflater.from(getContext())
+                .inflate(layoutResource, null, false);
+        setView(view, context);
+    }
+
+    private void setView(View view, Context context) {
+        setLayoutResource(R.layout.layout_preference_frame);
+        mRootView = view;
+        setShouldDisableView(false);
+        ButttonOne = findViewById(R.id.fodiconone_button);
+        ButttonTwo = findViewById(R.id.fodicontwo_button);
+        ButttonThree = findViewById(R.id.fodiconthree_button);
+
+        int defaultfodicon = Settings.System.getInt(
+                context.getContentResolver(), Settings.System.FOD_ICON, 0);
+        if (defaultfodicon==0) {
+            updateHighlightedItem(ButttonOne, context);
+        } else if (defaultfodicon==1) {
+            updateHighlightedItem(ButttonTwo, context);
+        } else if (defaultfodicon==2) {
+            updateHighlightedItem(ButttonThree, context);
+        }
+
+        ButttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateSettings(0, context);
+                updateHighlightedItem(ButttonOne, context);
+            }
+        });
+        ButttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateSettings(1, context);
+                updateHighlightedItem(ButttonTwo, context);
+            }
+        });
+        ButttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateSettings(2, context);
+                updateHighlightedItem(ButttonThree, context);
+            }
+        });
+    }
+
+    private void updateSettings(int fodicon, Context context) {
+        Settings.System.putInt(context.getContentResolver(), Settings.System.FOD_ICON, fodicon);
+    }
+
+    private void updateHighlightedItem(ImageButton activebutton, Context context) {
+        int defaultcolor = context.getResources().getColor(R.color.fod_item_background_stroke_color);
+        ColorStateList defaulttint = ColorStateList.valueOf(defaultcolor);
+        ButttonOne.setBackgroundTintList(defaulttint);
+        ButttonTwo.setBackgroundTintList(defaulttint);
+        ButttonThree.setBackgroundTintList(defaulttint);
+        activebutton.setBackgroundTintList(Utils.getColorAttr(getContext(), android.R.attr.colorAccent));
+    }
+}
