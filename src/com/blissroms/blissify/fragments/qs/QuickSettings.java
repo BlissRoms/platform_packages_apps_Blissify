83f5753b886d415cbb48ca2bfbba63ef2e4b1df1
Ido Ben-Hur <idoybh2@gmail.com>
2019-12-19 15:41:25 +0200

    Blissify: Allow customizing footer text [1/2]
    Change-Id: I04b3cb7915bd40bf4d224b4e634a5661bf73f377

--- src/com/blissroms/blissify/fragments/qs/QuickSettings.java
+++ src/com/blissroms/blissify/fragments/qs/QuickSettings.java
@@ -55,16 +55,21 @@ import java.util.Collections;
 import lineageos.providers.LineageSettings;
 import lineageos.preference.LineageSystemSettingListPreference;
 
+import com.bliss.support.preferences.SystemSettingEditTextPreference;
+
 public class QuickSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
+    private static final String BLISS_FOOTER_TEXT_STRING = "bliss_footer_text_string";
 
     private static final int PULLDOWN_DIR_NONE = 0;
     private static final int PULLDOWN_DIR_RIGHT = 1;
     private static final int PULLDOWN_DIR_LEFT = 2;
 
     private LineageSystemSettingListPreference mQuickPulldown;
+
+    private SystemSettingEditTextPreference mFooterString;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -79,6 +84,18 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                 (LineageSystemSettingListPreference) findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
         mQuickPulldown.setOnPreferenceChangeListener(this);
         updateQuickPulldownSummary(mQuickPulldown.getIntValue(0));
+
+        mFooterString = (SystemSettingEditTextPreference) findPreference(BLISS_FOOTER_TEXT_STRING);
+        mFooterString.setOnPreferenceChangeListener(this);
+        String footerString = Settings.System.getString(getContentResolver(),
+                BLISS_FOOTER_TEXT_STRING);
+        if (footerString != null && footerString != "")
+            mFooterString.setText(footerString);
+        else {
+            mFooterString.setText("#BlissRoms");
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.BLISS_FOOTER_TEXT_STRING, "#BlissRoms");
+        }
     }
 
     @Override
@@ -95,12 +112,23 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        int value = Integer.parseInt((String) newValue);
         String key = preference.getKey();
         switch (key) {
             case STATUS_BAR_QUICK_QS_PULLDOWN:
+                int value = Integer.parseInt((String) newValue);
                 updateQuickPulldownSummary(value);
                 break;
+            case BLISS_FOOTER_TEXT_STRING:
+            String value = (String) newValue;
+            if (value != "" && value != null)
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.BLISS_FOOTER_TEXT_STRING, value);
+            else {
+                mFooterString.setText("#BlissRoms");
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.BLISS_FOOTER_TEXT_STRING, "#BlissRoms");
+            }
+            break;
         }
         return true;
     }
