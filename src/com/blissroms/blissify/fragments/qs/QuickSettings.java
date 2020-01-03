1e53f6f0be24fc802dda11ba5b85d1b971c41d6c
Pranav Vashi <neobuddy89@gmail.com>
2020-01-03 06:38:22 +0530

    Separate battery estimate from percent for QS [2/2]
    Change-Id: I3a6e6b50536e4db90e37215c22c72596e71c0932

--- src/com/blissroms/blissify/fragments/qs/QuickSettings.java
+++ src/com/blissroms/blissify/fragments/qs/QuickSettings.java
@@ -183,7 +183,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_TILE_TITLE_VISIBILITY, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
-                Settings.System.QS_BATTERY_MODE, 1, UserHandle.USER_CURRENT);
+                Settings.System.QS_SHOW_BATTERY_PERCENT, 2, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_SHOW_BATTERY_ESTIMATE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.SHOW_QS_CLOCK, 1, UserHandle.USER_CURRENT);
         Settings.System.putInt(resolver,
