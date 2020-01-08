2ae27a8f0437cc51ed33fa53b93cf8c7d83fe219
rohan <purohit.rohan@gmail.com>
2020-01-08 20:26:54 +0530

    Lockscreen Clock Font Size [2/2]
    Change-Id: I8b31179a9ee299d462e7089a1f032a7176170c54

--- src/com/blissroms/blissify/fragments/lockscreen/LockScreen.java
+++ src/com/blissroms/blissify/fragments/lockscreen/LockScreen.java
@@ -130,6 +130,8 @@ public class LockScreen extends SettingsPreferenceFragment implements
                 Settings.System.LOCK_CLOCK_FONT_STYLE, 4, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.LOCK_DATE_FONT_STYLE, 14, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.LOCK_CLOCK_FONT_SIZE , 54, UserHandle.USER_CURRENT);
     }
 
     @Override
