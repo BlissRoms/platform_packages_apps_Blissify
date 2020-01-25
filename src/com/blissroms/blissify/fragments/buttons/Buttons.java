edf49f722d0f8ca20505798470f6c70b655ee8ea
Alex <du.alexcruz@gmail.com>
2020-01-25 15:01:32 -0500

    Blissify: Search index: Ensure our features show up under Settings
    Change-Id: I7edbfd8881869a9cc492121e925d7e6a44ff1181

--- src/com/blissroms/blissify/fragments/buttons/Buttons.java
+++ src/com/blissroms/blissify/fragments/buttons/Buttons.java
@@ -28,6 +28,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.RemoteException;
 import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Display;
@@ -48,6 +49,9 @@ import androidx.preference.DropDownPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
 import com.blissroms.blissify.utils.DeviceUtils;
 import com.blissroms.blissify.utils.TelephonyUtils;
 import org.lineageos.internal.util.ScreenType;
@@ -61,8 +65,9 @@ import lineageos.providers.LineageSettings;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+@SearchIndexable
 public class Buttons extends SettingsPreferenceFragment implements
-        OnPreferenceChangeListener {
+        OnPreferenceChangeListener, Indexable {
     private static final String TAG = "SystemSettings";
 
     private static final String KEY_BUTTON_BACKLIGHT = "button_backlight";
@@ -750,4 +755,25 @@ public class Buttons extends SettingsPreferenceFragment implements
         return MetricsProto.MetricsEvent.BLISSIFY;
     }
 
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.blissify_buttons;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
