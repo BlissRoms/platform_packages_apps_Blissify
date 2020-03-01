1d48a3bba219188706ec898a6a7e00657f6acc8c
rohan <purohit.rohan@gmail.com>
2020-03-01 14:44:58 +0200

    Blissify: Add RGB accent presets
    * Using the values of our existing accent overlays
    
    refernce: https://github.com/DerpLab/platform_packages_apps_DerpQuest/commit/618c6f8d099390ca9cc2e57a0c8338802510c184
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Id498812cd6941e88554fc62e0abc5ac9d6ae0afb
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- src/com/blissroms/blissify/fragments/themes/Themes.java
+++ src/com/blissroms/blissify/fragments/themes/Themes.java
@@ -57,12 +57,14 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import android.util.Log;
 
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
 
 import com.bliss.support.colorpicker.ColorPickerPreference;
+import com.blissroms.blissify.utils.DeviceUtils;
 
 @SearchIndexable
 public class Themes extends DashboardFragment  implements
@@ -70,9 +72,11 @@ public class Themes extends DashboardFragment  implements
 
     private static final String TAG = "Themes";
     private static final String ACCENT_COLOR = "accent_color";
+    private static final String ACCENT_PRESET = "accent_preset";
     static final int DEFAULT_ACCENT_COLOR = 0xff1a73e8;
 
     private ColorPickerPreference mAccentColor;
+    private ListPreference mAccentPreset;
 
     @Override
     protected int getPreferenceScreenResId() {
@@ -103,6 +107,9 @@ public class Themes extends DashboardFragment  implements
             mAccentColor.setSummary(hexColor);
         }
         mAccentColor.setNewPreviewColor(intColor);
+
+        mAccentPreset = (ListPreference) findPreference(ACCENT_PRESET);
+        mAccentPreset.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -152,6 +159,22 @@ public class Themes extends DashboardFragment  implements
             int intHex = ColorPickerPreference.convertToColorInt(hex);
             Settings.System.putIntForUser(resolver,
                     Settings.System.ACCENT_COLOR, intHex, UserHandle.USER_CURRENT);
+            return true;
+        } else if (preference == mAccentPreset) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+            String value = (String) newValue;
+            List<String> colorPresets = Arrays.asList(
+                    getResources().getStringArray(R.array.accent_presets_values));
+            int index = mAccentPreset.findIndexOfValue(value);
+            int color = DeviceUtils.convertToColorInt(colorPresets.get(index));
+            if (colorPresets.get(index).equals("ff1a73e8")) {
+                mAccentPreset.setSummary(R.string.default_string);
+            } else {
+                mAccentPreset.setSummary(mAccentPreset.getEntries()[index]);
+            }
+            //int color = Integer.parseInt(colorPresets.get(index));
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.ACCENT_COLOR, color, UserHandle.USER_CURRENT);
             return true;
         }
         return false;
