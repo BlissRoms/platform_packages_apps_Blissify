71da9df34fe01d9edd492c2707fbece4668eec85
Pranav Vashi <neobuddy89@gmail.com>
2020-06-28 17:15:41 +0530

    Blissify: Override VoLTE icon with VoWiFi when available [2/2]
    Change-Id: Ia1948ebb494826da7d43a824cbd82029758c736d

--- src/com/blissroms/blissify/fragments/statusbar/AdvancedStatusbar.java
+++ src/com/blissroms/blissify/fragments/statusbar/AdvancedStatusbar.java
@@ -66,6 +66,7 @@ public class AdvancedStatusbar extends SettingsPreferenceFragment implements
     private static final String KEY_OLD_MOBILETYPE = "use_old_mobiletype";
     private static final String BLISS_LOGO_COLOR = "status_bar_logo_color";
     private static final String KEY_VOWIFI_ICON_STYLE = "vowifi_icon_style";
+    private static final String KEY_VOLTE_VOWIFI_OVERRIDE = "volte_vowifi_override";
 
     private ListPreference mShowVolte;
     private SwitchPreference mDataDisabled;
@@ -74,6 +75,7 @@ public class AdvancedStatusbar extends SettingsPreferenceFragment implements
     private ColorPickerPreference mBlissLogoColor;
     private SwitchPreference mOldMobileType;
     private SystemSettingSeekBarPreference mVowifiIconStyle;
+    private SwitchPreference mOverride;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -91,6 +93,7 @@ public class AdvancedStatusbar extends SettingsPreferenceFragment implements
         mShowRoaming = (SwitchPreference) findPreference(KEY_SHOW_ROAMING);
         mShowFourg = (SwitchPreference) findPreference(KEY_SHOW_FOURG);
         mVowifiIconStyle = (SystemSettingSeekBarPreference) findPreference(KEY_VOWIFI_ICON_STYLE);
+        mOverride = (SwitchPreference) findPreference(KEY_VOLTE_VOWIFI_OVERRIDE);
 
         if (!TelephonyUtils.isVoiceCapable(getActivity())) {
             prefScreen.removePreference(mDataDisabled);
@@ -98,6 +101,7 @@ public class AdvancedStatusbar extends SettingsPreferenceFragment implements
             prefScreen.removePreference(mShowRoaming);
             prefScreen.removePreference(mShowFourg);
             prefScreen.removePreference(mVowifiIconStyle);
+            prefScreen.removePreference(mOverride);
         }
 
         mBlissLogoColor =
@@ -154,6 +158,8 @@ public class AdvancedStatusbar extends SettingsPreferenceFragment implements
         Settings.System.putIntForUser(resolver,
                 Settings.System.VOWIFI_ICON_STYLE, 0, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
+                Settings.System.VOLTE_VOWIFI_OVERRIDE, 1, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
                 Settings.System.DATA_DISABLED_ICON, 1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.ROAMING_INDICATOR_ICON, 1, UserHandle.USER_CURRENT);
@@ -177,5 +183,4 @@ public class AdvancedStatusbar extends SettingsPreferenceFragment implements
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.BLISSIFY;
     }
-
 }
