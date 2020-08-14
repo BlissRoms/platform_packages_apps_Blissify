33308ee69d79c9d03f8f4be55db4cd1a11c1ac03
rohan <purohit.rohan@gmail.com>
2020-08-14 09:49:05 -0400

    Revert "Revert "Add toggle to display battery in quick status bar [2/2]""
    This reverts commit d8727f8aa2c6b77f712209cb5102ab673d45f4c9.
    
    Change-Id: I6454063b5d9023288f9c0c22af1c3b1654c8b28b

--- src/com/blissroms/blissify/fragments/qs/QuickSettings.java
+++ src/com/blissroms/blissify/fragments/qs/QuickSettings.java
@@ -215,6 +215,8 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Settings.System.putInt(resolver,
                 Settings.System.BLUETOOTH_QS_SHOW_BATTERY, 1);
         Settings.System.putIntForUser(resolver,
+                Settings.System.QS_BATTERY_LOCATION, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
                 Settings.System.QS_BATTERY_STYLE, -1, UserHandle.USER_CURRENT);
         Settings.System.putIntForUser(resolver,
                 Settings.System.QS_DATAUSAGE, 0, UserHandle.USER_CURRENT);
