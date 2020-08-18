8a00a13c72597d5f8f8c82ff7c72eeb162b827cf
rohan <purohit.rohan@gmail.com>
2020-08-18 02:53:06 -0400

    Settings: categorise some more and organise
    * add logos to pref screen, keep it consistent
    * add search index to missing fragments
    
    Change-Id: I35f59176b4a3d064a0f2becf0fe7bdf8fad1be0e

--- src/com/blissroms/blissify/fragments/statusbar/CustomCarrierLabel.java
+++ src/com/blissroms/blissify/fragments/statusbar/CustomCarrierLabel.java
@@ -25,6 +25,7 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import android.text.InputFilter;
 import android.text.Spannable;
@@ -41,13 +42,20 @@ import com.android.internal.util.bliss.BlissUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settingslib.search.SearchIndexable;
 
 import com.bliss.support.preferences.SystemSettingListPreference;
 import com.bliss.support.colorpicker.ColorPickerPreference;
 import com.bliss.support.preferences.CustomSeekBarPreference;
 
+import java.util.List;
+import java.util.ArrayList;
+
+@SearchIndexable
 public class CustomCarrierLabel extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener, Indexable {
 
     public static final String TAG = "CarrierLabel";
     private static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
@@ -205,4 +213,25 @@ public class CustomCarrierLabel extends SettingsPreferenceFragment
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.BLISSIFY;
     }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.custom_carrier_label;
+                    result.add(sir);
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    return keys;
+                }
+    };
 }
