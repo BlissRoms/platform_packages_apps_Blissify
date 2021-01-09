f560dceea6699f7b5c76c22242f1fa9cff16a441
Lars Greiss <kufikugel@googlemail.com>
2021-01-09 07:01:37 +0100

    Blissify: Statusbar clock date customization (2/2)
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I286a19ae1e397c12920ad253f5cb955eeeda0522

--- src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
+++ src/com/blissroms/blissify/fragments/statusbar/StatusBar.java
@@ -83,8 +83,6 @@ public class StatusBar extends SettingsPreferenceFragment
 
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
 
-    private static final String CATEGORY_CLOCK = "status_bar_clock_key";
-
     private static final String ICON_BLACKLIST = "icon_blacklist";
     private static final String BLISS_LOGO_COLOR = "status_bar_logo_color";
 
@@ -183,7 +181,6 @@ public class StatusBar extends SettingsPreferenceFragment
             mBlissLogoColor.setSummary(R.string.default_string);
         }
         mBlissLogoColor.setOnPreferenceChangeListener(this);
-
 /*
         mOldMobileType = (SwitchPreference) findPreference(KEY_OLD_MOBILETYPE);
         boolean mConfigUseOldMobileType = mContext.getResources().getBoolean(
