19b9c3e7b59376689254ac1ba1331d9204f716e5
Pranav Vashi <neobuddy89@gmail.com>
2021-05-08 07:53:59 +0530

    Blissify: Add lockscreen background blur [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I26dce67e7d98b577a5c2d7ea4a1bf0e17c4f2596
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- src/com/blissroms/blissify/fragments/lockscreen/Lockscreen.java
+++ src/com/blissroms/blissify/fragments/lockscreen/Lockscreen.java
@@ -58,14 +58,20 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
 
+import com.bliss.support.preferences.SystemSettingSeekBarPreference;
+import com.blissroms.blissify.utils.Utils;
+
 @SearchIndexable
 public class Lockscreen extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener, Indexable {
 
     private static final String LOCKSCREEN_CATEGORY = "lockscreen_category";
     private static final String LOCKSCREEN_FOD_CATEGORY = "lockscreen_fod_category";
+    private static final String KEY_LOCKSCREEN_BLUR = "lockscreen_blur";
     private ContentResolver mResolver;
     private Preference FODSettings;
+    private Context mContext;
+    private SystemSettingSeekBarPreference mLockscreenBlur;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -79,6 +85,10 @@ public class Lockscreen extends SettingsPreferenceFragment implements
             prefScreen.removePreference(FODSettings);
         }
 
+        mLockscreenBlur = (SystemSettingSeekBarPreference) findPreference(KEY_LOCKSCREEN_BLUR);
+        if (!Utils.isBlurSupported()) {
+            prefScreen.removePreference(mLockscreenBlur);
+        }
     }
 
     @Override
