65149da81b82f3c6aa7278a4fe764c097ed43718
bigrushdog <bigrushdog@users.noreply.github.com>
2021-10-26 12:48:31 +0000

    Blissify: Hardware key: Customization support [2/2] Based on DUI
    jhenrique09 edits: make it DUI independent
    
    Original commit message:
    Same robust action library as used on software navigation. Supports
    single tap, double tap, and long press. As a precautionary measure,
    single tap back and single tap home are fixed and can not be changed.
    Camera button actions are not supported at this time. We will bring
    in wake key support at a later time.
    
    Also includes:
    
    Change-Id: I29ff38678821ca80db36d49d3b10d8ac29a6b4de
    DUI: Initial checkin for Oreo [4/7]

--- src/org/blissroms/blissify/fragments/Buttons.java
+++ src/org/blissroms/blissify/fragments/Buttons.java
@@ -20,6 +20,7 @@ import android.app.ActivityManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.RemoteException;
@@ -29,12 +30,19 @@ import android.provider.Settings;
 import android.util.Log;
 
 import androidx.fragment.app.DialogFragment;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
@@ -42,15 +50,38 @@ import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
+import com.android.settings.Utils;
 
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Toast;
 
+import com.android.internal.util.hwkeys.ActionConstants;
+import com.android.internal.util.hwkeys.ActionUtils;
+import org.blissroms.blissify.preferences.ActionFragment;
+
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
-public class Buttons extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
+public class Buttons extends ActionFragment implements OnPreferenceChangeListener {
+
+    // category keys
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+    private static final String CATEGORY_VOLUME = "volume_keys";
+    private static final String CATEGORY_POWER = "power_key";
+     // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+    public static final int KEY_MASK_VOLUME = 0x40;
+
     private static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
 
     private ListPreference mTorchPowerButton;
@@ -62,6 +93,49 @@ public class Buttons extends SettingsPreferenceFragment implements
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+         // bits for hardware keys present on device
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+         // read bits for present hardware keys
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasAppSwitchKey = (deviceKeys & KEY_MASK_APP_SWITCH) != 0;
+         // load categories and init/remove preferences based on device
+        // configuration
+        final PreferenceCategory backCategory = (PreferenceCategory) prefSet
+                .findPreference(CATEGORY_BACK);
+        final PreferenceCategory homeCategory = (PreferenceCategory) prefSet
+                .findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory = (PreferenceCategory) prefSet
+                .findPreference(CATEGORY_MENU);
+        final PreferenceCategory assistCategory = (PreferenceCategory) prefSet
+                .findPreference(CATEGORY_ASSIST);
+        final PreferenceCategory appSwitchCategory = (PreferenceCategory) prefSet
+                .findPreference(CATEGORY_APPSWITCH);
+         // back key
+        if (!hasBackKey) {
+            prefSet.removePreference(backCategory);
+        }
+         // home key
+        if (!hasHomeKey) {
+            prefSet.removePreference(homeCategory);
+        }
+         // App switch key (recents)
+        if (!hasAppSwitchKey) {
+            prefSet.removePreference(appSwitchCategory);
+        }
+         // menu key
+        if (!hasMenuKey) {
+            prefSet.removePreference(menuCategory);
+        }
+         // search/assist key
+        if (!hasAssistKey) {
+            prefSet.removePreference(assistCategory);
+        }
+         // let super know we can load ActionPreferences
+        onPreferenceScreenLoaded(ActionConstants.getDefaults(ActionConstants.HWKEYS));
 
         // screen off torch
         mTorchPowerButton = (ListPreference) findPreference(TORCH_POWER_BUTTON_GESTURE);
