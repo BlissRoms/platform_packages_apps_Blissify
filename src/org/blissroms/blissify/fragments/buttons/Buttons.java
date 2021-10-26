60e8b117dd68bd421f2fa3fda3967a27dda27758
GodfatherX64 <GodfatherX64@users.noreply.github.com>
2021-10-26 14:49:40 +0000

    Blissify: Hardware key: Enable single tap rebinding for hwkey back and home
    Initially, this policy was put in place to safeguard
    the UX by preventing a usecase where the device could
    be put in a state without back or home available. However,
    if a user puts the device in this state, it is easy enough
    to navigate to button settings to fix it
    
    Change-Id: I6b71f9e54fcb99356e07f236e9ed926ac5fc017e

--- src/org/blissroms/blissify/fragments/buttons/Buttons.java
+++ src/org/blissroms/blissify/fragments/buttons/Buttons.java
@@ -157,6 +157,11 @@ public class Buttons extends ActionFragment implements OnPreferenceChangeListene
     }
 
     @Override
+    protected boolean usesExtendedActionsList() {
+        return true;
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
     }
