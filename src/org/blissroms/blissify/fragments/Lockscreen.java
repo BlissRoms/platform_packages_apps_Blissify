61f9bfd38d4b9c2dfb532666071a040100ff2c52
idoybh <idoybh@users.noreply.github.com>
2021-12-11 00:13:54 +0000

    Blissify: Allow scheduling always on display [2/2] From sunset to sunrise or at a custom time
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Change-Id: I46c8969c6db6d6c9bb6dc7aa80b3e74be5c78c92

--- src/org/blissroms/blissify/fragments/Lockscreen.java
+++ src/org/blissroms/blissify/fragments/Lockscreen.java
@@ -58,6 +58,9 @@ public class Lockscreen extends SettingsPreferenceFragment implements
     private static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
     private static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
     private static final String UDFPS_CATEGORY = "udfps_category";
+    private static final String AOD_SCHEDULE_KEY = "always_on_display_schedule";
+
+    Preference mAODPref;
 
     private PreferenceCategory mUdfpsCategory;
 
@@ -100,8 +103,32 @@ public class Lockscreen extends SettingsPreferenceFragment implements
             prefSet.removePreference(mUdfpsCategory);
         }
 
+	mAODPref = findPreference(AOD_SCHEDULE_KEY);
+        updateAlwaysOnSummary();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateAlwaysOnSummary();
+    }
+
+    private void updateAlwaysOnSummary() {
+        if (mAODPref == null) return;
+        int mode = Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                Settings.Secure.DOZE_ALWAYS_ON_AUTO_MODE, 0, UserHandle.USER_CURRENT);
+        switch (mode) {
+            case 0:
+                mAODPref.setSummary(R.string.disabled);
+                break;
+            case 1:
+                mAODPref.setSummary(R.string.night_display_auto_mode_twilight);
+                break;
+            case 2:
+                mAODPref.setSummary(R.string.night_display_auto_mode_custom);
+                break;
+        }
+    }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
