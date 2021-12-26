ded15cbbce9061422fe47f12619d337651379d54
Pranav Vashi <neobuddy89@gmail.com>
2021-12-27 01:59:55 +0530

    Blissify: Add adaptive icon shape customization
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I44996a8517e76874a1049daf795f489066e943ad
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- src/org/blissroms/blissify/fragments/ThemeSettings.java
+++ src/org/blissroms/blissify/fragments/ThemeSettings.java
@@ -93,6 +93,8 @@ public class ThemeSettings extends DashboardFragment implements
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.font"));
         controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.icon_pack"));
         controllers.add(new OverlayCategoryPreferenceController(context,
                 "android.theme.customization.signal_icon"));
