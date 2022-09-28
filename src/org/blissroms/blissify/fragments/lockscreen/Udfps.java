319a9efdcfe31bbb4fd6b0f14dd042304e65483b
Jackeagle <jackeagle102@gmail.com>
2022-09-28 16:04:55 +0200

    Blissify: Add UDFPS Category
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: Ibb07dfcd3fc42738e5820445cd842ffc135f4c43
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/org/blissroms/blissify/fragments/lockscreen/Udfps.java
+++ src/org/blissroms/blissify/fragments/lockscreen/Udfps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014-2021 The BlissRoms Project
+ * Copyright (C) 2014-2022 The BlissRoms Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,94 +17,79 @@
 package org.blissroms.blissify.fragments.lockscreen;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.UserHandle;
+import android.os.ParcelFileDescriptor;
+import android.text.TextUtils;
 import android.app.WallpaperManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
 import android.os.UserHandle;
 import android.provider.Settings;
 
-import androidx.preference.SwitchPreference;
 import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceManager;
 import androidx.preference.PreferenceScreen;
-
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
-import com.android.internal.util.bliss.udfps.UdfpsUtils;
-
+import androidx.preference.SwitchPreference;
 import com.bliss.support.preferences.SystemSettingSwitchPreference;
-import com.bliss.support.preferences.CustomSeekBarPreference;
-import com.bliss.support.preferences.SystemSettingListPreference;
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.bliss.BlissUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.json.JSONException;
-import org.json.JSONObject;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import static android.os.UserHandle.USER_SYSTEM;
-import static android.os.UserHandle.USER_CURRENT;
+import java.io.FileDescriptor;
+import java.util.Arrays;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
-public class Lockscreen extends SettingsPreferenceFragment implements
+public class Udfps extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
-
-    private static final String UDFPS_CATEGORY = "udfps_category";
-
-    private Context mContext;
-    private PreferenceCategory mUdfpsCategory;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.blissify_lockscreen);
+        addPreferencesFromResource(R.xml.blissify_udfps);
 
-        final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
-        final PackageManager mPm = getActivity().getPackageManager();
+        Resources resources = getResources();
 
-        mUdfpsCategory = findPreference(UDFPS_CATEGORY);
-        if (!UdfpsUtils.hasUdfpsSupport(getContext())) {
-            prefSet.removePreference(mUdfpsCategory);
-        }
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        updateAlwaysOnSummary();
-    }
-
-    private void updateAlwaysOnSummary() {
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         return false;
     }
 
     @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
     public int getMetricsCategory() {
-        return MetricsEvent.BLISSIFY;
+        return MetricsProto.MetricsEvent.BLISSIFY;
     }
 
     /**
      * For Search.
      */
-
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.blissify_lockscreen);
+            new BaseSearchIndexProvider(R.xml.blissify_udfps);
 }
