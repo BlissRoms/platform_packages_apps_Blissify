3143ba1e497290bc155f5d05b0c6f0e975a0abc5
Rizky Benggolo <travarilo@gmail.com>
2023-04-13 05:51:42 +0200

    Revert "Revert "Gear: statusbar padding: correct the padding for big ass notch devices [2/2]""
    This reverts commit c3c39b07a711e49ccbb148b5fa823cda0de7117f.
    
    Change-Id: I0d24a5c812c7906f25ba4e4badc47ae75fd24cf1
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/org/blissroms/blissify/fragments/statusbar/Statusbar.java
+++ src/org/blissroms/blissify/fragments/statusbar/Statusbar.java
@@ -20,8 +20,10 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import android.os.Bundle;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
 import android.content.ContentResolver;
@@ -34,7 +36,6 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
-import android.text.format.DateFormat;
 import android.provider.Settings;
 import com.android.settings.R;
 
@@ -45,13 +46,14 @@ import com.bliss.support.preferences.SystemSettingSwitchPreference;
 import com.bliss.support.preferences.SystemSettingSeekBarPreference;
 import com.bliss.support.preferences.SecureSettingSwitchPreference;
 import com.bliss.support.preferences.SystemSettingListPreference;
-import com.bliss.support.preferences.SecureSettingListPreference;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settings.Utils;
 import android.util.Log;
 import com.bliss.support.preferences.SystemSettingSwitchPreference;
+import com.bliss.support.preferences.CustomSeekBarPreference;
+import com.bliss.support.preferences.SystemSettingSeekBarPreference;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -63,19 +65,23 @@ import java.util.Collections;
 public class Statusbar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_STATUS_BAR_AM_PM = "status_bar_am_pm";
-    private static final String BATTERY_STYLE = "status_bar_battery_style";
-    private static final String SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-    private static final String SHOW_BATTERY_PERCENT_CHARGING = "status_bar_show_battery_percent_charging";
-    private static final String SHOW_BATTERY_PERCENT_INSIDE = "status_bar_show_battery_percent_inside";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
+    private static final String STATUSBAR_LEFT_PADDING = "statusbar_left_padding";
+    private static final String STATUSBAR_RIGHT_PADDING = "statusbar_right_padding";
 
+    private static final int BATTERY_STYLE_PORTRAIT = 0;
+    private static final int BATTERY_STYLE_TEXT = 2;
+    private static final int BATTERY_STYLE_HIDDEN = 3;
+
+    private SwitchPreference mBatteryTextCharging;
+    private SystemSettingListPreference mBatteryPercent;
     private SystemSettingListPreference mBatteryStyle;
-    private SystemSettingSwitchPreference mBatteryPercent;
-    private SystemSettingSwitchPreference mBatteryPercentCharging;
-    private SystemSettingSwitchPreference mBatteryPercentInside;
-    private SecureSettingListPreference mStatusBarAmPm;
-    private SystemSettingSwitchPreference mThreshold;
+    private CustomSeekBarPreference mThreshold;
     private SystemSettingSwitchPreference mNetMonitor;
+    private SystemSettingSeekBarPreference mSbLeftPadding;
+    private SystemSettingSeekBarPreference mSbRightPadding;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -84,8 +90,26 @@ public class Statusbar extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+        Context mContext = getActivity().getApplicationContext();
+        Resources res = null;
+        Context ctx = getContext();
 
-	mStatusBarAmPm = findPreference(KEY_STATUS_BAR_AM_PM);
+        int batterystyle = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
+        int batterypercent = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
+
+        mBatteryStyle = (SystemSettingListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        mBatteryStyle.setOnPreferenceChangeListener(this);
+
+        mBatteryPercent = (SystemSettingListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+        mBatteryPercent.setEnabled(
+                batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
+        mBatteryPercent.setOnPreferenceChangeListener(this);
+
+        mBatteryTextCharging = (SwitchPreference) findPreference(STATUS_BAR_BATTERY_TEXT_CHARGING);
+        mBatteryTextCharging.setEnabled(batterystyle == BATTERY_STYLE_HIDDEN ||
+                (batterystyle != BATTERY_STYLE_TEXT && batterypercent != 2));
 
         boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
                 Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
@@ -93,41 +117,56 @@ public class Statusbar extends SettingsPreferenceFragment implements
         mNetMonitor.setChecked(isNetMonitorEnabled);
         mNetMonitor.setOnPreferenceChangeListener(this);
 
-        boolean isThresholdEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 0, UserHandle.USER_CURRENT) == 1;
-        mThreshold = (SystemSettingSwitchPreference) findPreference("network_traffic_autohide_threshold");
-        mThreshold.setChecked(isThresholdEnabled);
-        mThreshold.setOnPreferenceChangeListener(this);
-
-        mBatteryPercent = findPreference(SHOW_BATTERY_PERCENT);
-        final boolean percentEnabled = Settings.System.getIntForUser(resolver,
-                SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT) == 1;
-        mBatteryPercent.setChecked(percentEnabled);
-        mBatteryPercent.setOnPreferenceChangeListener(this);
-
-        mBatteryPercentInside = findPreference(SHOW_BATTERY_PERCENT_INSIDE);
-        mBatteryPercentInside.setEnabled(percentEnabled);
-        final boolean percentInside = Settings.System.getIntForUser(resolver,
-                SHOW_BATTERY_PERCENT_INSIDE, 0, UserHandle.USER_CURRENT) == 1;
-        mBatteryPercentInside.setChecked(percentInside);
-        mBatteryPercentInside.setOnPreferenceChangeListener(this);
-
-        mBatteryStyle = findPreference(BATTERY_STYLE);
         int value = Settings.System.getIntForUser(resolver,
-                BATTERY_STYLE, 0, UserHandle.USER_CURRENT);
-        mBatteryStyle.setValue(Integer.toString(value));
-        mBatteryStyle.setSummary(mBatteryStyle.getEntry());
-        mBatteryStyle.setOnPreferenceChangeListener(this);
-        updatePercentEnablement(value != 2);
+                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 1, UserHandle.USER_CURRENT);
+        mThreshold = (CustomSeekBarPreference) findPreference("network_traffic_autohide_threshold");
+        mThreshold.setValue(value);
+        mThreshold.setOnPreferenceChangeListener(this);
+        mThreshold.setEnabled(isNetMonitorEnabled);
+        
+        float density = Resources.getSystem().getDisplayMetrics().density;
 
-        mBatteryPercentCharging = findPreference(SHOW_BATTERY_PERCENT_CHARGING);
-        updatePercentChargingEnablement(value, percentEnabled, percentInside);
+        try {
+            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        mSbLeftPadding = (SystemSettingSeekBarPreference) findPreference(STATUSBAR_LEFT_PADDING);
+        int sbLeftPadding = Settings.System.getIntForUser(ctx.getContentResolver(),
+                Settings.System.LEFT_PADDING, ((int) (res.getIdentifier("com.android.systemui:dimen/status_bar_padding_start", null, null) / density)), UserHandle.USER_CURRENT);
+        mSbLeftPadding.setValue(sbLeftPadding);
+        mSbLeftPadding.setOnPreferenceChangeListener(this);
+
+        mSbRightPadding = (SystemSettingSeekBarPreference) findPreference(STATUSBAR_RIGHT_PADDING);
+        int sbRightPadding = Settings.System.getIntForUser(ctx.getContentResolver(),
+                Settings.System.RIGHT_PADDING, ((int) (res.getIdentifier("com.android.systemui:dimen/status_bar_padding_end", null, null) / density)), UserHandle.USER_CURRENT);
+        mSbRightPadding.setValue(sbRightPadding);
+        mSbRightPadding.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-         if (preference == mNetMonitor) {
+        Resources res = null;
+        Context ctx = getContext();
+
+        if (preference == mBatteryStyle) {
+            int value = Integer.parseInt((String) newValue);
+            int batterypercent = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
+            mBatteryPercent.setEnabled(
+                    value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
+            mBatteryTextCharging.setEnabled(value == BATTERY_STYLE_HIDDEN ||
+                    (value != BATTERY_STYLE_TEXT && batterypercent != 2));
+            return true;
+        } else if (preference == mBatteryPercent) {
+            int value = Integer.parseInt((String) newValue);
+            int batterystyle = Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
+            mBatteryTextCharging.setEnabled(batterystyle == BATTERY_STYLE_HIDDEN ||
+                    (batterystyle != BATTERY_STYLE_TEXT && value != 2));
+            return true;
+        } else if (preference == mNetMonitor) {
             boolean value = (Boolean) newValue;
             Settings.System.putIntForUser(getActivity().getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_STATE, value ? 1 : 0,
@@ -136,63 +175,45 @@ public class Statusbar extends SettingsPreferenceFragment implements
             mThreshold.setEnabled(value);
             return true;
         } else if (preference == mThreshold) {
-            boolean value = (Boolean) newValue;
+            int val = (Integer) newValue;
             Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, value ? 1 : 0,
+                    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, val,
                     UserHandle.USER_CURRENT);
             return true;
-        } else if (preference == mBatteryStyle) {
-            int value = Integer.valueOf((String) newValue);
-            int index = mBatteryStyle.findIndexOfValue((String) newValue);
-            mBatteryStyle.setSummary(mBatteryStyle.getEntries()[index]);
-            Settings.System.putIntForUser(resolver,
-                    BATTERY_STYLE, value, UserHandle.USER_CURRENT);
-            updatePercentEnablement(value != 2);
-            updatePercentChargingEnablement(value, null, null);
+        }
+       float density = Resources.getSystem().getDisplayMetrics().density;
+
+        try {
+            res = ctx.getPackageManager().getResourcesForApplication("com.android.systemui");
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (preference == mSbLeftPadding) {
+            int leftValue = (Integer) newValue;
+            int sbLeft = ((int) (leftValue / density));
+            Settings.System.putIntForUser(getContext().getContentResolver(),
+                    Settings.System.LEFT_PADDING, sbLeft, UserHandle.USER_CURRENT);
             return true;
-        } else if (preference == mBatteryPercent) {
-            boolean enabled = (boolean) newValue;
-            Settings.System.putInt(resolver,
-                    SHOW_BATTERY_PERCENT, enabled ? 1 : 0);
-            mBatteryPercentInside.setEnabled(enabled);
-            updatePercentChargingEnablement(null, enabled, null);
+        } else if (preference == mSbRightPadding) {
+            int rightValue = (Integer) newValue;
+            int sbRight = ((int) (rightValue / density));
+            Settings.System.putIntForUser(getContext().getContentResolver(),
+                    Settings.System.RIGHT_PADDING, sbRight, UserHandle.USER_CURRENT);
             return true;
-        } else if (preference == mBatteryPercentInside) {
-            boolean enabled = (boolean) newValue;
-            Settings.System.putInt(resolver,
-                    SHOW_BATTERY_PERCENT_INSIDE, enabled ? 1 : 0);
-            // we already know style isn't text and percent is enabled
-            mBatteryPercentCharging.setEnabled(enabled);
+        } else if (preference == mSbTopPadding) {
+            int topValue = (Integer) newValue;
+            int sbTop = ((int) (topValue / density));
+            Settings.System.putIntForUser(getContext().getContentResolver(),
+                    Settings.System.TOP_PADDING, sbTop, UserHandle.USER_CURRENT);
             return true;
         }
        return false;
     }
 
-    private void updatePercentEnablement(boolean enabled) {
-        mBatteryPercent.setEnabled(enabled);
-        mBatteryPercentInside.setEnabled(enabled && mBatteryPercent.isChecked());
-    }
-
-    private void updatePercentChargingEnablement(Integer style, Boolean percent, Boolean inside) {
-        if (style == null) style = Integer.valueOf(mBatteryStyle.getValue());
-        if (percent == null) percent = mBatteryPercent.isChecked();
-        if (inside == null) inside = mBatteryPercentInside.isChecked();
-        mBatteryPercentCharging.setEnabled(style != 2 && (!percent || inside));
-    }
-
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.BLISSIFY;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        if (DateFormat.is24HourFormat(requireContext())) {
-            mStatusBarAmPm.setEnabled(false);
-            mStatusBarAmPm.setSummary(R.string.status_bar_am_pm_unavailable);
-        }
     }
 
     /**
