555cabaf6d28303e12dc5fde0d0439ef7286b3af
Jis G Jacob <studiokeys@blissroms.org>
2023-11-05 01:47:19 -0400

    Blissify: Initial Cleanup for Universe
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/org/blissroms/blissify/fragments/statusbar/Statusbar.java
+++ src/org/blissroms/blissify/fragments/statusbar/Statusbar.java
@@ -61,25 +61,6 @@ import java.util.Collections;
 public class Statusbar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String KEY_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
-    private static final String KEY_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
-    private static final String KEY_STATUS_BAR_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
-
-    private static final int BATTERY_STYLE_PORTRAIT = 0;
-    private static final int BATTERY_STYLE_TEXT = 4;
-    private static final int BATTERY_STYLE_HIDDEN = 5;
-
-    private static final int BATTERY_PERCENT_HIDDEN = 0;
-    private static final int BATTERY_PERCENT_INSIDE = 1;
-    private static final int BATTERY_PERCENT_RIGHT = 2;
-    private static final int BATTERY_PERCENT_LEFT = 3;
-
-    private SwitchPreference mBatteryTextCharging;
-    private SystemSettingListPreference mBatteryPercent;
-    private SystemSettingListPreference mBatteryStyle;
-    private SystemSettingSwitchPreference mThreshold;
-    private SystemSettingSwitchPreference mNetMonitor;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -88,92 +69,12 @@ public class Statusbar extends SettingsPreferenceFragment implements
         PreferenceScreen prefSet = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
 
-        boolean isNetMonitorEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_STATE, 1, UserHandle.USER_CURRENT) == 1;
-        mNetMonitor = (SystemSettingSwitchPreference) findPreference("network_traffic_state");
-        mNetMonitor.setChecked(isNetMonitorEnabled);
-        mNetMonitor.setOnPreferenceChangeListener(this);
-
-        boolean isThresholdEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, 0, UserHandle.USER_CURRENT) == 1;
-        mThreshold = (SystemSettingSwitchPreference) findPreference("network_traffic_autohide_threshold");
-        mThreshold.setChecked(isThresholdEnabled);
-        mThreshold.setOnPreferenceChangeListener(this);
-
-        int batterystyle = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-        int batterypercent = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
-
-        mBatteryStyle = (SystemSettingListPreference) findPreference(KEY_STATUS_BAR_BATTERY_STYLE);
-        mBatteryStyle.setOnPreferenceChangeListener(this);
-
-        mBatteryPercent = (SystemSettingListPreference) findPreference(KEY_STATUS_BAR_SHOW_BATTERY_PERCENT);
-        mBatteryPercent.setOnPreferenceChangeListener(this);
-
-        handleBatteryPercent(batterystyle, batterypercent);
-
-        mBatteryTextCharging = (SwitchPreference) findPreference(KEY_STATUS_BAR_BATTERY_TEXT_CHARGING);
-        mBatteryTextCharging.setEnabled(batterystyle != BATTERY_STYLE_TEXT &&
-                (batterypercent == BATTERY_PERCENT_INSIDE || batterypercent == BATTERY_PERCENT_HIDDEN));
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-         if (preference == mNetMonitor) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putIntForUser(getActivity().getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_STATE, value ? 1 : 0,
-                    UserHandle.USER_CURRENT);
-            mNetMonitor.setChecked(value);
-            mThreshold.setEnabled(value);
-            return true;
-        } else if (preference == mThreshold) {
-            boolean value = (Boolean) newValue;
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD, value ? 1 : 0,
-                    UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == mBatteryStyle) {
-            int batterystyle = Integer.parseInt((String) newValue);
-            int batterypercent = Settings.System.getIntForUser(getContentResolver(),
-                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
-            handleBatteryPercent(batterystyle, batterypercent);
-            mBatteryTextCharging.setEnabled(batterystyle != BATTERY_STYLE_TEXT &&
-                    (batterypercent == BATTERY_PERCENT_INSIDE || batterypercent == BATTERY_PERCENT_HIDDEN));
-            return true;
-        } else if (preference == mBatteryPercent) {
-            int batterypercent = Integer.parseInt((String) newValue);
-            int batterystyle = Settings.System.getIntForUser(getContentResolver(),
-                    Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-            mBatteryTextCharging.setEnabled(batterystyle != BATTERY_STYLE_TEXT &&
-                    (batterypercent == BATTERY_PERCENT_INSIDE || batterypercent == BATTERY_PERCENT_HIDDEN));
-            return true;
-        }
        return false;
-    }
-
-    private void handleBatteryPercent(int batterystyle, int batterypercent) {
-        if (batterystyle < BATTERY_STYLE_TEXT) {
-            mBatteryPercent.setEntries(R.array.status_bar_battery_percent_entries);
-            mBatteryPercent.setEntryValues(R.array.status_bar_battery_percent_values);;
-        }
-        else {
-            mBatteryPercent.setEntries(R.array.status_bar_battery_percent_no_text_inside_entries);
-            mBatteryPercent.setEntryValues(R.array.status_bar_battery_percent_no_text_inside_values);
-            if (batterypercent == BATTERY_PERCENT_INSIDE) {
-                batterypercent = BATTERY_PERCENT_HIDDEN;
-                mBatteryPercent.setValueIndex(BATTERY_PERCENT_HIDDEN);
-            }
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT,
-                    batterypercent, UserHandle.USER_CURRENT);
-        }
-
-        mBatteryPercent.setEnabled(
-                batterystyle != BATTERY_STYLE_TEXT &&
-                batterystyle != BATTERY_STYLE_HIDDEN);
     }
 
     @Override
