6c0c035ab21de8d0721dc6860f609a84406da294
SagarMakhar <sagarmakhar@gmail.com>
2024-05-13 23:28:31 +0000

    Blissify: UDFPS icons
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/org/blissroms/blissify/fragments/themes/ThemeSettings.java
+++ src/org/blissroms/blissify/fragments/themes/ThemeSettings.java
@@ -68,12 +68,14 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
     private static final String KEY_ICONS_CATEGORY = "themes_icons_category";
     private static final String KEY_NAVBAR_ICON = "android.theme.customization.navbar";
     private static final String KEY_SIGNAL_ICON = "android.theme.customization.signal_icon";
+    private static final String KEY_UDFPS_ICON = "udfps_icon";
     private static final String KEY_ANIMATIONS_CATEGORY = "themes_animations_category";
     private static final String KEY_UDFPS_ANIMATION = "udfps_animation";
 
     private PreferenceCategory mIconsCategory;
     private Preference mNavbarIcon;
     private Preference mSignalIcon;
+    private Preference mUdfpsIcon;
     private PreferenceCategory mAnimationsCategory;
     private Preference mUdfpsAnimation;
 
@@ -89,6 +91,7 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
         mNavbarIcon = (Preference) findPreference(KEY_NAVBAR_ICON);
         mSignalIcon = (Preference) findPreference(KEY_SIGNAL_ICON);
+        mUdfpsIcon = (Preference) findPreference(KEY_UDFPS_ICON);
         mAnimationsCategory = (PreferenceCategory) findPreference(KEY_ANIMATIONS_CATEGORY);
         mUdfpsAnimation = (Preference) findPreference(KEY_UDFPS_ANIMATION);
 
@@ -104,8 +107,12 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
                 getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
 
         if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+            mIconsCategory.removePreference(mUdfpsIcon);
             mAnimationsCategory.removePreference(mUdfpsAnimation);
         } else {
+            if (!BlissUtils.isPackageInstalled(context, "org.blissroms.udfps.icons")) {
+                mIconsCategory.removePreference(mUdfpsIcon);
+            }
             if (!BlissUtils.isPackageInstalled(context, "org.blissroms.udfps.animations")) {
                 mAnimationsCategory.removePreference(mUdfpsAnimation);
             }
@@ -151,8 +158,12 @@ public class ThemeSettings extends DashboardFragment implements OnPreferenceChan
                 }
 
                 if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+                    keys.add(KEY_UDFPS_ICON);
                     keys.add(KEY_UDFPS_ANIMATION);
                 } else {
+                    if (!BlissUtils.isPackageInstalled(context, "org.blissroms.udfps.icons")) {
+                        keys.add(KEY_UDFPS_ICON);
+                    }
                     if (!BlissUtils.isPackageInstalled(context, "org.blissroms.udfps.animations")) {
                         keys.add(KEY_UDFPS_ANIMATION);
                     }
