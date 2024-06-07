4a577c25af290a5f4b57e0e1fbdfd8bc9801adbb
minaripenguin <minaripenguin@users.noreply.github.com>
2024-06-07 16:42:08 +0900

    Blissify: A11 QS UI Style [3/3]
    [neobuddy89: Remove contentobserver, not useful]
    
    Change-Id: I3a94b5f626b025bf9cdd9fbeb6af26cd3d1ddf4b
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/org/blissroms/blissify/fragments/qs/QuickSettings.java
+++ src/org/blissroms/blissify/fragments/qs/QuickSettings.java
@@ -41,6 +41,7 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.bliss.ThemeUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -57,12 +58,21 @@ import java.util.List;
 public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String KEY_QS_UI_STYLE  = "qs_tile_ui_style";
+
     private ListPreference mQuickPulldown;
+    private ListPreference mQsUI;
+
+    private static ThemeUtils mThemeUtils;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.blissify_quicksettings);
+
+        final Context context = getContext();
+
+        mThemeUtils = new ThemeUtils(getActivity());
 
         int qpmode = Settings.System.getIntForUser(getContentResolver(),
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 0, UserHandle.USER_CURRENT);
@@ -70,6 +80,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mQuickPulldown.setValue(String.valueOf(qpmode));
         mQuickPulldown.setSummary(mQuickPulldown.getEntry());
         mQuickPulldown.setOnPreferenceChangeListener(this);
+
+        mQsUI = (ListPreference) findPreference(KEY_QS_UI_STYLE);
+        mQsUI.setOnPreferenceChangeListener(this);
+
+        checkQSOverlays(context);
     }
 
     @Override
@@ -85,7 +100,54 @@ public class QuickSettings extends SettingsPreferenceFragment implements
                     mQuickPulldown.getEntries()[index]);
             return true;
         }
+        if (preference == mQsUI) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.QS_TILE_UI_STYLE, value, UserHandle.USER_CURRENT);
+            updateQsStyle(getActivity());
+            checkQSOverlays(getActivity());
+            return true;
+        }
         return false;
+    }
+
+    private static void updateQsStyle(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+
+        boolean isA11Style = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_UI_STYLE , 0, UserHandle.USER_CURRENT) != 0;
+
+        String qsUIStyleCategory = "android.theme.customization.qs_ui";
+        String overlayThemeTarget  = "com.android.systemui";
+        String overlayThemePackage  = "com.android.system.qs.ui.A11";
+
+        if (mThemeUtils == null) {
+            mThemeUtils = new ThemeUtils(context);
+        }
+
+        // reset all overlays before applying
+        mThemeUtils.setOverlayEnabled(qsUIStyleCategory, overlayThemeTarget, overlayThemeTarget);
+
+        if (isA11Style) {
+            mThemeUtils.setOverlayEnabled(qsUIStyleCategory, overlayThemePackage, overlayThemeTarget);
+        }
+    }
+
+    private void checkQSOverlays(Context context) {
+        ContentResolver resolver = context.getContentResolver();
+        int isA11Style = Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_UI_STYLE , 0, UserHandle.USER_CURRENT);
+
+        if (isA11Style > 0) {
+            mQsUI.setEnabled(true);
+        } else {
+            mQsUI.setEnabled(true);
+        }
+
+        // Update summaries
+        int index = mQsUI.findIndexOfValue(Integer.toString(isA11Style));
+        mQsUI.setValue(Integer.toString(isA11Style));
+        mQsUI.setSummary(mQsUI.getEntries()[index]);
     }
 
     @Override
