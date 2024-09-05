30771e3a9fba8450d3f918b6a76fa90ba2d24b4a
AnierinB <anierin@evolution-x.org>
2024-09-05 02:37:23 +0000

    Blissify: Add SystemUtils for systemui restart dialog
    Change-Id: I9db14c03844de1fb5542f3cd85c20043ab2c839d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ src/org/blissroms/blissify/utils/SystemUtils.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2025 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.blissroms.blissify.utils;
+
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.IActivityManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+
+import com.android.settings.R;
+
+public class SystemUtils {
+
+    public static void showSystemUiRestartDialog(final Context context) {
+        new AlertDialog.Builder(context)
+                .setTitle(R.string.systemui_restart_title)
+                .setMessage(R.string.systemui_restart_message)
+                .setPositiveButton(R.string.systemui_restart_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        new AsyncTask<Void, Void, Void>() {
+                            @Override
+                            protected Void doInBackground(Void... params) {
+                                try {
+                                    ActivityManager am =
+                                            (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+                                    IActivityManager ams = ActivityManager.getService();
+                                    for (ActivityManager.RunningAppProcessInfo app : am.getRunningAppProcesses()) {
+                                        if ("com.android.systemui".equals(app.processName)) {
+                                            ams.killApplicationProcess(app.processName, app.uid);
+                                            break;
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                                return null;
+                            }
+                        }.execute();
+                    }
+                })
+                .setNegativeButton(R.string.systemui_restart_not_now, null)
+                .show();
+    }
+}
