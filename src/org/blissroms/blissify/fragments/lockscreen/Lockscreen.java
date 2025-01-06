ea1e52e2e526aee6cb360ec2b2a94fabd131d71d
Pranav Vashi <neobuddy89@gmail.com>
2025-01-06 02:57:28 -0500

    Blissify: Add proper toggle for smartspace [2/2]
    VOS: disabled by default
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/org/blissroms/blissify/fragments/lockscreen/Lockscreen.java
+++ src/org/blissroms/blissify/fragments/lockscreen/Lockscreen.java
@@ -69,6 +69,7 @@ public class Lockscreen extends SettingsPreferenceFragment implements
     private static final String FINGERPRINT_SUCCESS_VIB = "fingerprint_success_vib";
     private static final String FINGERPRINT_ERROR_VIB = "fingerprint_error_vib";
     private static final String KEY_WEATHER = "lockscreen_weather_enabled";
+    private static final String KEY_SMARTSPACE = "lockscreen_smartspace_enabled";
 
     private Context mContext;
     private FingerprintManager mFingerprintManager;
@@ -76,6 +77,7 @@ public class Lockscreen extends SettingsPreferenceFragment implements
     private SwitchPreferenceCompat mFingerprintErrorVib;
     private PreferenceCategory mUdfpsCategory;
     private Preference mWeather;
+    private SwitchPreferenceCompat mSmartspace;
     private OmniJawsClient mWeatherClient;
 
     @Override
@@ -107,6 +109,10 @@ public class Lockscreen extends SettingsPreferenceFragment implements
             prefSet.removePreference(mFingerprintSuccessVib);
             prefSet.removePreference(mFingerprintErrorVib);
         }
+
+        mSmartspace = (SwitchPreferenceCompat) findPreference(KEY_SMARTSPACE);
+        mSmartspace.setOnPreferenceChangeListener(this);
+
         mWeather = (Preference) findPreference(KEY_WEATHER);
         mWeatherClient = new OmniJawsClient(getContext());
         updateWeatherSettings();
@@ -130,16 +136,20 @@ public class Lockscreen extends SettingsPreferenceFragment implements
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.FP_ERROR_VIBRATE, value ? 1 : 0);
             return true;
+        } else if (preference == mSmartspace) {
+            mSmartspace.setChecked((Boolean)newValue);
+            updateWeatherSettings();
+            return true;
         }
         return false;
     }
 
     private void updateWeatherSettings() {
-        if (mWeatherClient == null || mWeather == null) return;
+        if (mWeatherClient == null || mWeather == null || mSmartspace == null) return;
 
         boolean weatherEnabled = mWeatherClient.isOmniJawsEnabled();
-        mWeather.setEnabled(weatherEnabled);
-        mWeather.setSummary(weatherEnabled ? R.string.lockscreen_weather_summary :
+        mWeather.setEnabled(!mSmartspace.isChecked() && weatherEnabled);
+        mWeather.setSummary(!mSmartspace.isChecked() && weatherEnabled ? R.string.lockscreen_weather_summary :
             R.string.lockscreen_weather_enabled_info);
     }
 
