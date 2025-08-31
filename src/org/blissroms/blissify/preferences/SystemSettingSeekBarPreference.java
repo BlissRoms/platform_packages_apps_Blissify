dafa90b2917716ed6df9346a27941a87d4c5b7da
Pranav Vashi <neobuddy89@gmail.com>
2025-08-31 20:50:19 +0530

    Blissify: Update CustomSeekbarPreference to use material expressive layout
    Change-Id: I47d8f9cbe5875d51dda8b31bedb0ca562c0d5516
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/org/blissroms/blissify/preferences/SystemSettingSeekBarPreference.java
+++ src/org/blissroms/blissify/preferences/SystemSettingSeekBarPreference.java
@@ -1,6 +1,17 @@
 /*
- * Copyright (C) 2018-2019 crDroid Android Project
- * SPDX-License-Identifier: Apache-2.0
+ * Copyright (C) 2016-2025 crDroid Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.blissroms.blissify.preferences;
@@ -9,11 +20,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 
 public class SystemSettingSeekBarPreference extends CustomSeekBarPreference {
-
-    public SystemSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
-    }
 
     public SystemSettingSeekBarPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
