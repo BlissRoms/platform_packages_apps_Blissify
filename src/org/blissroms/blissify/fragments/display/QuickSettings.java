b7cfbc91268cdb43a78b8186108b6c35acb785cd
Jackeagle <jackeagle102@gmail.com>
2026-03-18 19:39:55 +0530

    Blissify: Add QS columns and rows settings
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I6a0e9d9be85dedb227901b4e6851ce4af70e8b93

--- src/org/blissroms/blissify/fragments/display/QuickSettings.java
+++ src/org/blissroms/blissify/fragments/display/QuickSettings.java
@@ -27,6 +27,7 @@ import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import org.blissroms.blissify.fragments.BlissifyDashboardFragment;
+import org.blissroms.blissify.preferences.SecureSeekBarPreference;
 
 @SearchIndexable(forTarget = SearchIndexable.ALL & ~SearchIndexable.ARC)
 public class QuickSettings extends BlissifyDashboardFragment
@@ -36,8 +37,14 @@ public class QuickSettings extends BlissifyDashboardFragment
 
   private static final String KEY_QS_PANEL_STYLE = "qs_panel_style";
   private static final String KEY_TILE_LABEL_HIDE = "qs_tile_label_hide";
+  private static final String KEY_TILE_COLUMNS = "qs_tile_columns";
+  private static final String KEY_QQS_ROWS = "qs_tile_qqs_rows";
+  private static final String KEY_QS_ROWS = "qs_tile_qs_rows";
 
   private Preference mTileLabelHide;
+  private SecureSeekBarPreference mTileColumns;
+  private SecureSeekBarPreference mQqsRows;
+  private SecureSeekBarPreference mQsRows;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -45,6 +52,23 @@ public class QuickSettings extends BlissifyDashboardFragment
 
     Preference stylePref = findPreference(KEY_QS_PANEL_STYLE);
     mTileLabelHide = findPreference(KEY_TILE_LABEL_HIDE);
+    mTileColumns = findPreference(KEY_TILE_COLUMNS);
+    mQqsRows = findPreference(KEY_QQS_ROWS);
+    mQsRows = findPreference(KEY_QS_ROWS);
+
+    android.content.res.Resources res = getContext().getResources();
+    if (mTileColumns != null) {
+      mTileColumns.updateMax(res.getInteger(
+          com.android.internal.R.integer.config_qsTileColumnsMax));
+    }
+    if (mQqsRows != null) {
+      mQqsRows.updateMax(res.getInteger(
+          com.android.internal.R.integer.config_qsTileQqsRowsMax));
+    }
+    if (mQsRows != null) {
+      mQsRows.updateMax(res.getInteger(
+          com.android.internal.R.integer.config_qsTileQsRowsMax));
+    }
 
     if (stylePref != null) {
       stylePref.setOnPreferenceChangeListener(this);
@@ -66,6 +90,9 @@ public class QuickSettings extends BlissifyDashboardFragment
 
   private void updateCircularPrefs(boolean circular) {
     if (mTileLabelHide != null) mTileLabelHide.setVisible(circular);
+    if (mTileColumns != null) mTileColumns.setVisible(circular);
+    if (mQqsRows != null) mQqsRows.setVisible(circular);
+    if (mQsRows != null) mQsRows.setVisible(circular);
   }
 
   @Override
